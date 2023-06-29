285e2d8ce38a05c1d078bd3e21f3671b8667f7ba
maparent <marcandre.parent@rossvideo.com>
2023-06-29 16:39:48 -0400

    Added minimal Catena Client-Server implementations with a Main.

---
+++ sdks/java/src/main/java/com/rossvideo/catena/example/MyCatenaClient.java
@@ -0,0 +1,101 @@
+package com.rossvideo.catena.example;
+
+import catena.core.CatenaServiceGrpc;
+import catena.core.CatenaServiceGrpc.CatenaServiceBlockingStub;
+import catena.core.DeviceRequestPayload;
+import catena.core.GetValuePayload;
+import catena.core.SetValuePayload;
+import catena.core.Value;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
+
+public class MyCatenaClient implements AutoCloseable {
+
+	private final String hostname;
+	private final int port;
+
+	private CatenaServiceBlockingStub stub;
+	private ManagedChannel channel;
+
+	public MyCatenaClient(String hostname, int port) {
+		this(hostname, port, 1);
+	}
+
+	public MyCatenaClient(String hostname, int port, int slotNumber) {
+		this.hostname = hostname;
+		this.port = port;
+	}
+
+	public void start() {
+		if (channel == null) {
+			ManagedChannelBuilder<?> channelBuilder = ManagedChannelBuilder.forAddress(hostname, port).usePlaintext();
+			channel = channelBuilder.build();
+			stub = CatenaServiceGrpc.newBlockingStub(channel);
+		}
+	}
+
+	@Override
+	public void close() throws Exception {
+		System.out.println("CLIENT: Shutting down Client.");
+		if (channel != null) {
+			channel.shutdownNow();
+		}
+		channel = null;
+		stub = null;
+	}
+
+	public void setValue(String oid, int slotNumber, int value) {
+		printSetValueMessage("int", value);
+		setValue(oid, slotNumber, Value.newBuilder().setInt32Value(value).build());
+	}
+	
+	public void setValue(String oid, int slotNumber, float value) {
+		printSetValueMessage("float", value);
+		setValue(oid, slotNumber, Value.newBuilder().setFloat32Value(value).build());
+	}
+	
+	public void setValue(String oid, int slotNumber, Value newValue) {
+		try {
+			stub.setValue(SetValuePayload.newBuilder().setOid(oid).setSlot(slotNumber).setValue(newValue).build());
+		} catch (StatusRuntimeException exception) {
+			printStatusRuntimeException("setValue", exception);
+		}
+	}
+	
+	public Value getValue(String oid, int slotNumber) {
+		try {
+			Value value = stub.getValue(GetValuePayload.newBuilder().setOid(oid).setSlot(slotNumber).build());
+			printGetValueResult("Value", value);
+			return value;
+		}  catch (StatusRuntimeException exception) {
+			printStatusRuntimeException("getValue", exception);
+		}
+		return null;
+	}
+	
+	public void getDevice(int slotNumber) {
+		try {
+			stub.deviceRequest(DeviceRequestPayload.newBuilder().setSlot(slotNumber).build()).forEachRemaining(deviceComponent -> {
+				printGetValueResult("DeviceComponent", deviceComponent);
+			});
+			
+		}  catch (StatusRuntimeException exception) {
+			printStatusRuntimeException("getDevice", exception);
+		}
+	}
+	
+	private void printSetValueMessage(String valueType, Object value) {
+		System.out.println("CLIENT: Setting " + valueType + " value: " + value);
+	}
+	
+	private void printGetValueResult(String valueType, Object result) {
+		System.out.println("CLIENT: Received " + valueType + ": " + result);
+	}
+	
+	private void printStatusRuntimeException(String methodName, StatusRuntimeException exception) {
+		System.err.println("CLIENT: " + methodName + " RPC failed: " + exception.getStatus() + '\n');
+		System.err.flush();
+	}
+	
+}
---
+++ sdks/java/src/main/java/com/rossvideo/catena/example/MyCatenaDevice.java
@@ -0,0 +1,147 @@
+package com.rossvideo.catena.example;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import com.google.protobuf.Empty;
+import com.rossvideo.catena.example.error.InvalidSlotNumberException;
+import com.rossvideo.catena.example.error.UnknownOidException;
+import com.rossvideo.catena.example.error.WrongValueTypeException;
+
+import catena.core.CatenaServiceGrpc.CatenaServiceImplBase;
+import catena.core.Device;
+import catena.core.DeviceComponent;
+import catena.core.DeviceRequestPayload;
+import catena.core.GetValuePayload;
+import catena.core.Param;
+import catena.core.ParamDescriptor;
+import catena.core.ParamType;
+import catena.core.ParamType.Type;
+import catena.core.PolyglotText;
+import catena.core.SetValuePayload;
+import catena.core.Value;
+import catena.core.Value.KindCase;
+import io.grpc.stub.StreamObserver;
+
+public class MyCatenaDevice extends CatenaServiceImplBase {
+
+	public static final String INT_OID = "integer";
+	public static final String FLOAT_OID = "float";
+	
+	private static final AtomicInteger deviceId = new AtomicInteger(1);
+	
+	private Value intValue = Value.newBuilder().setInt32Value(0).build();
+	private Value floatValue = Value.newBuilder().setFloat32Value(0F).build();
+
+	private final String deviceName = MyCatenaDevice.class.getSimpleName() + '-' + deviceId.getAndIncrement();
+	private final int slot;
+
+	public MyCatenaDevice(int slot) {
+		this.slot = slot;
+	}
+
+	@Override
+	public void deviceRequest(DeviceRequestPayload request, StreamObserver<DeviceComponent> responseObserver) {
+		int slot = request.getSlot();
+		System.out.println("SERVER: deviceRequest request for slot: " + slot);
+		if (this.slot != slot) {
+			responseObserver.onError(new InvalidSlotNumberException(slot));
+			return;
+		}
+		
+		Device device = Device.newBuilder().setSlot(this.slot).putAllParams(getAllParameters()).build();
+		DeviceComponent deviceComponent = DeviceComponent.newBuilder().setDevice(device).build();
+		responseObserver.onNext(deviceComponent);
+		responseObserver.onCompleted();
+	}
+	
+	private Map<String, ParamDescriptor> getAllParameters() {
+		Map<String, ParamDescriptor> parameters = new HashMap<>();
+		parameters.put(FLOAT_OID, buildParamDescriptor(FLOAT_OID, "float parameter", Type.FLOAT32, false, 1, floatValue));
+		parameters.put(INT_OID, buildParamDescriptor(INT_OID, "int parameter", Type.INT32, false, 1, intValue));
+		return parameters;
+	}
+	
+	private ParamDescriptor buildParamDescriptor(String oid, String name, Type type, boolean readonly, int precision, Value value) {
+		Param param = Param.newBuilder()
+				.setFqoid(deviceName + '.' + oid)
+				.setName(PolyglotText.newBuilder().setMonoglot(name).build())
+				.setType(ParamType.newBuilder().setType(type).build())
+				.setReadOnly(readonly)
+				.setPrecision(precision)
+				.setValue(value)
+				.build();
+		return ParamDescriptor.newBuilder().setParam(param).build();
+	}
+	
+	@Override
+	public void setValue(SetValuePayload request, StreamObserver<Empty> responseObserver) {
+		System.out.println("SERVER: setValue request:");
+		System.out.println("\toid:   " + request.getOid());
+		System.out.println("\tslot:  " + request.getSlot());
+		System.out.println("\tvalue: " + request.getValue());
+		int slot = request.getSlot();
+		if (this.slot != slot) {
+			responseObserver.onError(new InvalidSlotNumberException(slot));
+			return;
+		}
+
+		String oid = request.getOid();
+		Value value = request.getValue();
+		KindCase valueKindCase = value.getKindCase();
+		switch (oid) {
+			case INT_OID:
+				if (valueKindCase != KindCase.INT32_VALUE) {
+					responseObserver.onError(new WrongValueTypeException(oid, valueKindCase, KindCase.INT32_VALUE));
+					return;
+				}
+				intValue = value;
+				break;
+			case FLOAT_OID:
+				if (valueKindCase != KindCase.FLOAT32_VALUE) {
+					responseObserver.onError(new WrongValueTypeException(oid, valueKindCase, KindCase.FLOAT32_VALUE));
+					return;
+				}
+				floatValue = value;
+				break;
+			default:
+				responseObserver.onError(new UnknownOidException(oid));
+				return;
+		}
+
+		responseObserver.onNext(Empty.getDefaultInstance());
+		responseObserver.onCompleted();
+	}
+
+	@Override
+	public void getValue(GetValuePayload request, StreamObserver<Value> responseObserver) {
+		System.out.println("SERVER: getValue request:");
+		System.out.println("\toid:  " + request.getOid());
+		System.out.println("\tslot: " + request.getSlot());
+		System.out.println();
+		int slot = request.getSlot();
+		if (this.slot != slot) {
+			responseObserver.onError(new InvalidSlotNumberException(slot));
+			return;
+		}
+
+		Value value;
+		String oid = request.getOid();
+		switch (oid) {
+		case INT_OID:
+			value = intValue;
+			break;
+		case FLOAT_OID:
+			value = floatValue;
+			break;
+		default:
+			responseObserver.onError(new UnknownOidException(oid));
+			return;
+		}
+
+		responseObserver.onNext(value);
+		responseObserver.onCompleted();
+	}
+
+}
