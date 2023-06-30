0557e1468e8b901ab4bf982033a25a86766d8fc5
maparent <marcandre.parent@rossvideo.com>
2023-06-30 15:34:15 -0400

    Rename the file which runs the Server and Client in one go.

--- sdks/java/src/main/java/com/rossvideo/catena/example/main/ServerClientMain.java
+++ sdks/java/src/main/java/com/rossvideo/catena/example/main/ServerClientMain.java
@@ -1,27 +1,30 @@
-package com.rossvideo.catena.example;
+package com.rossvideo.catena.example.main;
+
+import com.rossvideo.catena.example.MyCatenaClient;
+import com.rossvideo.catena.example.MyCatenaDevice;
 
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 
-public class Main {
+public class ServerClientMain {
 
 	private static int port = 5255;
 	private static int slotNumber = 1;
 	private static boolean run = true;
 	private static Object lock = new Object();
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		new Thread(() -> {
 			try {
 				Server server = ServerBuilder.forPort(port).addService(new MyCatenaDevice(slotNumber)).build();
 				server.start();
-				
-				while (run) {
-					synchronized(lock) {
+
+				synchronized (lock) {
+					while (run) {
 						lock.wait();
 					}
 				}
-				
+
 				System.out.println("SERVER: Shutting down Server.");
 				server.shutdownNow();
 				server.awaitTermination();
@@ -30,52 +33,54 @@ public class Main {
 			}
 		}).start();
 
+		Thread.sleep(2000); // Let some time for the server to start first
+		
 		new Thread(() -> {
-			
+
 			try (MyCatenaClient client = new MyCatenaClient("localhost", port)) {
 				client.start();
-				
+
 				// Tests for int parameter
 				System.out.println("TEST: Read initial int value.");
 				client.getValue(MyCatenaDevice.INT_OID, slotNumber);
-				
+
 				int intValue = 42;
 				System.out.println("TEST: Set int value to " + intValue);
 				client.setValue(MyCatenaDevice.INT_OID, slotNumber, intValue);
-				
+
 				System.out.println("TEST: Read int value.");
 				client.getValue(MyCatenaDevice.INT_OID, slotNumber);
-				
+
 				// Tests for float parameter
 				System.out.println("TEST: Read initial float value.");
 				client.getValue(MyCatenaDevice.FLOAT_OID, slotNumber);
-				
+
 				float floatValue = 42.42F;
 				System.out.println("TEST: Set float value to " + floatValue);
 				client.setValue(MyCatenaDevice.FLOAT_OID, slotNumber, floatValue);
-				
+
 				System.out.println("TEST: Read float value.");
 				client.getValue(MyCatenaDevice.FLOAT_OID, slotNumber);
-				
+
 				// Tests for set/get parameter error cases
 				String wrongOid = "blah_blah_blah";
 				System.out.println("TEST: Read value with wrong oid: " + wrongOid);
 				client.getValue(wrongOid, slotNumber);
-				
+
 				int wrongSlotNumber = 12345;
 				System.out.println("TEST: Read value with wrong slot number: " + wrongSlotNumber);
 				client.getValue(MyCatenaDevice.INT_OID, wrongSlotNumber);
-				
+
 				System.out.println("TEST: Setting int value on a float type OID.");
 				client.setValue(MyCatenaDevice.FLOAT_OID, slotNumber, 42);
-				
+
 				// Tests for getDevice
 				System.out.println("TEST: GetDevice for slot: " + slotNumber);
 				client.getDevice(slotNumber);
-				
+
 				System.out.println("TEST: GetDevice for wrong slot: " + wrongSlotNumber);
 				client.getDevice(wrongSlotNumber);
-				
+
 				System.out.println("TEST: Complete.");
 
 			} catch (Exception e) {
@@ -83,7 +88,7 @@ public class Main {
 				System.err.flush();
 				e.printStackTrace();
 			} finally {
-				synchronized(lock) {
+				synchronized (lock) {
 					run = false;
 					lock.notifyAll();
 				}
