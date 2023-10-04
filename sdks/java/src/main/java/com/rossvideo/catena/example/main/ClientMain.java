d2d55f4b78818e0b29b9569486b6648b9e0cbb35
maparent <marcandre.parent@rossvideo.com>
2023-10-04 15:18:36 -0400

    Move client test commands to a single static method

--- sdks/java/src/main/java/com/rossvideo/catena/example/main/ClientMain.java
+++ sdks/java/src/main/java/com/rossvideo/catena/example/main/ClientMain.java
@@ -1,7 +1,6 @@
 package com.rossvideo.catena.example.main;
 
 import com.rossvideo.catena.example.MyCatenaClient;
-import com.rossvideo.catena.example.MyCatenaDevice;
 
 public class ClientMain {
 
@@ -12,52 +11,7 @@ public class ClientMain {
         try (MyCatenaClient client = new MyCatenaClient("127.0.0.1", port)) {
             client.start();
 
-            // Tests for int parameter
-            System.out.println("TEST: Read initial int value.");
-            client.getValue(MyCatenaDevice.INT_OID, slotNumber);
-
-            int intValue = 42;
-            System.out.println("TEST: Set int value to " + intValue);
-            client.setValue(MyCatenaDevice.INT_OID, slotNumber, intValue);
-
-            System.out.println("TEST: Read int value.");
-            client.getValue(MyCatenaDevice.INT_OID, slotNumber);
-
-            // Tests for float parameter
-            System.out.println("TEST: Read initial float value.");
-            client.getValue(MyCatenaDevice.FLOAT_OID, slotNumber);
-
-            float floatValue = 42.42F;
-            System.out.println("TEST: Set float value to " + floatValue);
-            client.setValue(MyCatenaDevice.FLOAT_OID, slotNumber, floatValue);
-
-            System.out.println("TEST: Read float value.");
-            client.getValue(MyCatenaDevice.FLOAT_OID, slotNumber);
-
-            // Tests for set/get parameter error cases
-            String wrongOid = "blah_blah_blah";
-            System.out.println("TEST: Read value with wrong oid: " + wrongOid);
-            client.getValue(wrongOid, slotNumber);
-
-            int wrongSlotNumber = 12345;
-            System.out.println("TEST: Read value with wrong slot number: " + wrongSlotNumber);
-            client.getValue(MyCatenaDevice.INT_OID, wrongSlotNumber);
-
-            System.out.println("TEST: Setting int value on a float type OID.");
-            client.setValue(MyCatenaDevice.FLOAT_OID, slotNumber, 42);
-
-            // Tests for commands
-            System.out.println("TEST: Execute command \"foo('bar')\" with string value");
-            client.executeCommand("foo", slotNumber, "bar", true);
-
-            // Tests for getDevice
-            System.out.println("TEST: GetDevice for slot: " + slotNumber);
-            client.getDevice(slotNumber);
-
-            System.out.println("TEST: GetDevice for wrong slot: " + wrongSlotNumber);
-            client.getDevice(wrongSlotNumber);
-
-            System.out.println("TEST: Complete.");
+            CommandExecutor.executeOnClient(client, slotNumber);
 
         } catch (Exception e) {
             System.err.println("Exception occurred." + e.getLocalizedMessage());
