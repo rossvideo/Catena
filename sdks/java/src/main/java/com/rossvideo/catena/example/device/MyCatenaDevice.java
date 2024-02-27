625e1df4e28fc1801af6510255846dcbd28b8107
jpeltzer <jpeltzer@L-OTTJPELTZER3.rossvideo.com>
2024-02-26 21:47:10 -0500

    Fix SDK to remove menu group nesting an numeric IDs

--- sdks/java/src/main/java/com/rossvideo/catena/example/device/MyCatenaDevice.java
+++ sdks/java/src/main/java/com/rossvideo/catena/example/device/MyCatenaDevice.java
@@ -57,12 +57,12 @@ public class MyCatenaDevice extends BasicCatenaDevice {
     }
 
     private void buildMenus() {
-        getMenuManager().createMenuGroup("config", 1, "Config");
-        getMenuManager().createMenu("config", "config", 0, "Configuration");
+        getMenuManager().createMenuGroup("config", "Config");
+        getMenuManager().createMenu("config", "config", "Configuration");
         getMenuManager().addParamsMenu("config", "config", new String[] {FLOAT_OID, INT_OID, CHOICE_OID});;
         
-        getMenuManager().createMenuGroup("status", 0, "Status");
-        getMenuManager().createMenu("status", "product", 0, "Product");
+        getMenuManager().createMenuGroup("status", "Status");
+        getMenuManager().createMenu("status", "product", "Product");
         getMenuManager().addParamsMenu("status", "product", new String[] {DATE_AND_TIME_OID, FLOAT_OID, INT_OID, CHOICE_OID});
     }
     
