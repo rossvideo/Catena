c899c4a2d21d0be7f1e18bd57d2b3c582323ff23
John R. Naylor <128176657+mejohnnaylor@users.noreply.github.com>
2024-07-22 13:49:56 -0500

    Add UI (#203)
    * add a DB GUI
    
    * added classes to support Polyglot Text
    
    * codegen for name element of Param
    
    ---------
    
    Signed-off-by: John R. Naylor <128176657+mejohnnaylor@users.noreply.github.com>
    Co-authored-by: John Naylor <jnaylor@rossvideo.com>

--- sdks/cpp/lite/include/Param.h
+++ sdks/cpp/lite/include/Param.h
@@ -3,6 +3,7 @@
 #include <lite/include/IParam.h>
 #include <lite/include/Device.h>
 #include <lite/include/StructInfo.h>
+#include <lite/include/PolyglotText.h>
 
 #include <lite/param.pb.h>
 
@@ -12,6 +13,8 @@
 
 namespace catena {
 namespace lite {
+
+
 
 /**
  * @brief Param provides convenient access to parameter values
@@ -58,9 +61,9 @@ template <typename T> class Param : public IParam {
     /**
      * @brief the main constructor
      */
-    Param(catena::ParamType type, T& value, const OidAliases& oid_aliases, const std::string& oid,
+    Param(catena::ParamType type, T& value, const OidAliases& oid_aliases, const PolyglotText::ListInitializer name, const std::string& oid,
           Device& dm)
-        : IParam(oid), type_{type}, value_{value}, oid_aliases_{oid_aliases}, dm_{dm} {
+        : IParam(oid), type_{type}, value_{value}, oid_aliases_{oid_aliases}, name_{name}, dm_{dm} {
         dm.addItem<Device::ParamTag>(oid, this, Device::ParamTag{});
     }
 
@@ -88,6 +91,11 @@ template <typename T> class Param : public IParam {
         for (const auto& oid_alias : oid_aliases_) {
             param.add_oid_aliases(oid_alias);
         }
+        catena::PolyglotText name_proto;
+        for (const auto& [lang, text] : name_.displayStrings()) {
+            (*name_proto.mutable_display_strings())[lang] = text;
+        }
+        param.mutable_name()->Swap(&name_proto);
         toProto(*param.mutable_value());
     }
 
@@ -96,9 +104,30 @@ template <typename T> class Param : public IParam {
      */
     ParamType type() const override { return type_; }
 
+    /**
+     * @brief get the parameter name
+     */
+    const PolyglotText::DisplayStrings& name() const { return name_.displayStrings(); }
+
+    /**
+     * @brief get the parameter name by language
+     * @param language the language to get the name for
+     * @return the name in the specified language, or an empty string if the language is not found
+     */
+    const std::string& name(const std::string& language) const { 
+      auto it = name_.displayStrings().find(language);
+      if (it != name_.displayStrings().end()) {
+        return it->second;
+      } else {
+        static const std::string empty;
+        return empty;
+      }
+    }
+
   private:
     ParamType type_;  // ParamType is from param.pb.h
     std::vector<std::string> oid_aliases_;
+    PolyglotText name_;
     std::reference_wrapper<T> value_;
     std::reference_wrapper<Device> dm_;
 };
