652d2e6b4c0ab074f3ebdfd95786bf5c88465f81
John Danen <john.danen@rossvideo.com>
2024-07-31 16:31:57 -0400

    Improved template example

--- sdks/cpp/lite/examples/use_templates/use_templates.cpp
+++ sdks/cpp/lite/examples/use_templates/use_templates.cpp
@@ -14,7 +14,7 @@
 
 /**
  * @file use_structs.cpp
- * @author john.naylor@rossvideo.com
+ * @author john.danen@rossvideo.com
  * @brief Steps up the complexity a notch by dealing with structured data
  *
  * It does not support any connections so is not a complete example
@@ -25,7 +25,7 @@
  */
 
 
-#include "lite/examples/use_structs/device.use_structs.json.h"  // dm
+#include "lite/examples/use_templates/device.use_templates.json.h"  // dm
 #include <lite/include/Device.h>
 #include <lite/include/Param.h>
 #include <lite/include/PolyglotText.h>
@@ -33,69 +33,27 @@
 
 using namespace catena::lite;
 using namespace catena::common;
-using namespace use_structs;
+using namespace use_templates;
 #include <iostream>
 int main() {
     // lock the model
     Device::LockGuard lg(dm);
 
-    auto& locationParam = *dynamic_cast<Param<Location>*>(dm.getItem("/location", Device::ParamTag()));
-    Location& locationValue = locationParam.get();
-    std::cout << "Location from model - latitude: " << locationValue.latitude
-              << ", longitude: " << locationValue.longitude << std::endl;
-    // create another Location object, using the default initial value specified in the model
-    Location externalToModel;
-    std::cout << "Location object external to model - latitude: " << externalToModel.latitude
-              << ", longitude: " << externalToModel.longitude << std::endl;
-    locationValue = externalToModel;
+    Param<City>& canadasCapital = *dynamic_cast<Param<City>*>(dm.getItem("/ottawa", Device::ParamTag()));
+    City& city = canadasCapital.get();
+    std::cout << "Canada's capital city is " << city.city_name
+              << " at latitude " << city.latitude
+              << " and longitude " << city.longitude
+              << " with a population of " << city.population
+              << std::endl;
 
-
-    // serialize the location parameter - will be removed from this example
-    catena::Value value{};
-    locationParam.toProto(value);
-
-    catena::patterns::EnumDecorator<catena::ParamType> type{locationParam.type()};
-    std::cout << "location type: " << type.toString() << std::endl;
-
-    // print the serialized value
-    std::cout << "Location name: " << locationParam.name("en") << std::endl;
-    auto& struct_value = value.struct_value();
-    auto& fields = struct_value.fields();
-    for (const auto& field : fields) {
-        std::cout << "Field: " << field.first << std::endl;
-        auto& field_value = field.second.value();
-        if (field_value.has_float32_value()) {
-            std::cout << "float32: " << field_value.float32_value() << std::endl;
-        } else {
-            std::cout << "Unknown type" << std::endl;
-        }
-    }
-
-    catena::Param param{};
-    locationParam.toProto(param);
-    auto& aliases = param.oid_aliases();
-    for (const auto& alias : aliases) {
-        std::cout << "Alias: " << alias << std::endl;
-    }
-    auto& name = param.name();
-    for (const auto& [lang, text] : name.display_strings()) {
-        std::cout << "Language: " << lang << ", Text: " << text << std::endl;
-    }
-
-
-    catena::Device dstDevice{};
-    dm.toProto(dstDevice, false); // select the deep copy option
-    std::cout << "Device model serialized to protobuf" << std::endl;
-
-
-    // deserialize the Location object - will be removed from this example
-    locationParam.get() = Location{0, 0};
-    std::cout << "Location from model cleared to 0 - latitude: " << locationParam.get().latitude
-              << ", longitude: " << locationParam.get().longitude << std::endl;
-    locationParam.fromProto(value);
-    std::cout << "Location from model set with protobuf deserialization - latitude: " << locationParam.get().latitude
-              << ", longitude: " << locationParam.get().longitude << std::endl;
-
+    Param<City>& ontariosCapital = *dynamic_cast<Param<City>*>(dm.getItem("/toronto", Device::ParamTag()));
+    City& city2 = ontariosCapital.get();
+    std::cout << "Ontario's capital city is " << city2.city_name
+              << " at latitude " << city2.latitude
+              << " and longitude " << city2.longitude
+              << " with a population of " << city2.population
+              << std::endl;
    
     return EXIT_SUCCESS;
 }
