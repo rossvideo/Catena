60b29fae492398d780b692c1c90e08b8912a19de
BeanJ4m <thehuntingblade5@gmail.com>
2024-10-08 00:22:50 -0400

    Initial commit for the Menu and MenuGroup interfaces

---
+++ sdks/cpp/common/include/IMenu.h
@@ -0,0 +1,79 @@
+#pragma once
+
+/**THIS IS A COPY OF ILanguagePack.h NOT READY FOR RELEASE */
+
+/** Copyright 2024 Ross Video Ltd
+
+ Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+ 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+ 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+ 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+ INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+//
+
+/**
+ * @file IMenu.h
+ * @brief Interface for Menus
+ * @author Ben Mostafa Ben.Mostafa@rossvideo.com
+ * @date 2024-10-04
+ * @copyright Copyright (c) 2024 Ross Video
+ */
+
+//
+#include <string>
+#include <unordered_map>
+
+namespace catena {
+
+class Menu; // forward reference
+
+namespace common {
+
+/**
+ * @brief Interface for Language Packs
+ */
+class IMenu {
+public:
+    /**
+     * @brief Const iterator to the key/word pairs
+     */
+    using const_iterator = std::unordered_map<std::string, std::string>::const_iterator;
+
+public:
+    IMenu() = default;
+    IMenu(IMenu&&) = default;
+    IMenu& operator=(IMenu&&) = default;
+    virtual ~IMenu() = default;
+
+    /**
+     * @brief serialize a language pack to a protobuf message
+     * @param pack the protobuf message
+     */
+    virtual void toProto(catena::Menu& pack) const = 0;
+
+    /**
+     * @brief deserialize a language pack from a protobuf message
+     * @param pack the protobuf message
+     */
+    virtual void fromProto(const catena::Menu& pack) = 0;
+
+    /**
+     * get the begin iterator to the key/word pairs
+     */
+    virtual const_iterator begin() const = 0;
+
+    /**
+     * get the end iterator to the key/word pairs
+     */
+    virtual const_iterator end() const = 0;
+};
+}  // namespace common
+}  // namespace catena
---
+++ sdks/cpp/lite/include/Menu.h
@@ -0,0 +1,123 @@
+#pragma once
+
+/**THIS IS A COPY OF LanguagePack.h NOT READY FOR RELEASE */
+
+/** Copyright 2024 Ross Video Ltd
+
+ Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+ 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+ 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+ 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+ INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+ INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+//
+
+/**
+ * @file Menu.h
+ * @brief Menus support
+ * @author Ben Mostafa Ben.Mostafa@rossvideo.com
+ * @date 2024-10-04
+ * @copyright Copyright (c) 2024 Ross Video
+ */
+
+//common
+#include <IMenu.h>
+
+// lite
+#include <Device.h>
+
+// protobuf interface
+#include <interface/Menu.pb.h> //Unsure where to create this file
+
+#include <string>
+#include <unordered_map>
+#include <initializer_list>
+#include <vector>
+
+namespace catena {
+namespace lite {
+
+class Menu;  // forward declaration
+
+/**
+ * @brief Menu list
+ */
+class Menu : public common::IMenu {
+  public:
+    /**
+     * @brief Menu list initializer
+     */
+    using ListInitializer = std::initializer_list<std::pair<std::string, std::string>>;
+
+  public:
+    Menu() = delete;
+    /**
+     * @brief Menu does not have copy semantics
+     */
+    Menu(const Menu&) = delete;
+
+    /**
+     * @brief Menu has move semantics
+     */
+    Menu(Menu&&) = default;
+
+    /**
+     * @brief Menu does not have copy semantics
+     */
+    Menu& operator=(const Menu&) = delete;
+
+    /**
+     * @brief Menu has move semantics
+     */
+    Menu& operator=(Menu&&) = default;
+
+    /**
+     * destructor
+     */
+    virtual ~Menu() = default;
+
+    /**
+     * @brief construct a language pack from a list of words
+     * @param name the name of the language
+     * @param list the list of key/word pairs
+     * @param dev the device model to which this language pack belongs
+     */
+    Menu(const std::string& name, ListInitializer list, Device& dev);
+
+    /**
+     * @brief deserialize a language pack from a protobuf message
+     * @param pack the protobuf message
+     */
+    void fromProto(const ::catena::Menu& pack) override;
+
+    /**
+     * @brief serialize a language pack to a protobuf message
+     * @param pack the protobuf message
+     */
+    void toProto(::catena::Menu& pack) const override;
+
+    /**
+     * get the begin iterator to the key/word pairs
+     */
+    inline const_iterator begin() const override{ return words_.cbegin(); }
+
+    /**
+     * get the end iterator to the key/word pairs
+     */
+    inline const_iterator end() const override { return words_.cend(); }
+
+  private:
+    std::string name_;
+    std::unordered_map<std::string, std::string> words_;
+};
+
+
+}  // namespace lite
+}  // namespace catena
