f870a01de177a09fa0186e3a79da5eca0691c346
BenWhitten-RV <Benjamin.Whitten@rossvideo.com>
2025-01-16 14:47:37 +0000

    Seperated GetParam, still commented out in main code. Otherwise just some cleanup

--- sdks/cpp/connections/gRPC/src/ServiceImpl.cpp
+++ sdks/cpp/connections/gRPC/src/ServiceImpl.cpp
@@ -1,17 +1,32 @@
-// Copyright 2024 Ross Video Ltd
-//
-// Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
-//
-// 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-//
-// 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
-//
-// 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
-//
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, 
-// INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
-// INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
-// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+/*
+ * Copyright 2024 Ross Video Ltd
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS”
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * RE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 
 
 // common
@@ -19,13 +34,14 @@
 
 // connections/gRPC
 #include <ServiceImpl.h>
-#include <SetValue.h>
-#include <GetValue.h>
 #include <GetPopulatedSlots.h>
+#include <GetValue.h>
+#include <SetValue.h>
 #include <Connect.h>
 #include <DeviceRequest.h>
-#include <ExecuteCommand.h>
+// #include <GetParam.h>
 #include <ExternalObjectRequest.h>
+#include <ExecuteCommand.h>
 
 // type aliases
 using catena::common::ParamTag;
@@ -159,89 +175,3 @@ std::vector<std::string> CatenaServiceImpl::getScopes(ServerContext &context) {
     }
     return scopes;
 }
-
-
-
-
-    //  /**
-    //  * @brief CallData class for the GetParam RPC
-    //  */
-    // class GetParam : public CallData {
-    //   public:
-    //     GetParam(CatenaServiceImpl *service, Device &dm, bool ok)
-    //         : service_{service}, dm_{dm}, writer_(&context_),
-    //           status_{ok ? CallStatus::kCreate : CallStatus::kFinish} {
-    //         service->registerItem(this);
-    //         objectId_ = objectCounter_++;
-    //         proceed(service, ok);  // start the process
-    //     }
-    //     ~GetParam() {}
-
-    //     void proceed(CatenaServiceImpl *service, bool ok) override {
-    //         std::cout << "GetParam proceed[" << objectId_ << "]: " << timeNow()
-    //                   << " status: " << static_cast<int>(status_) << ", ok: " << std::boolalpha << ok
-    //                   << std::endl;
-
-    //         if(!ok){
-    //             std::cout << "GetParam[" << objectId_ << "] cancelled\n";
-    //             status_ = CallStatus::kFinish;
-    //         }
-            
-    //         switch (status_) {
-    //             case CallStatus::kCreate:
-    //                 status_ = CallStatus::kProcess;
-    //                 service_->RequestGetParam(&context_, &req_, &writer_, service_->cq_, service_->cq_,
-    //                                                this);
-    //                 break;
-
-    //             case CallStatus::kProcess:
-    //                 new GetParam(service_, dm_, ok);  // to serve other clients
-    //                 context_.AsyncNotifyWhenDone(this);
-    //                 clientScopes_ = getScopes(context_);
-    //                 status_ = CallStatus::kWrite;
-    //                 // fall thru to start writing
-
-    //             case CallStatus::kWrite:
-    //                 try {
-    //                     std::cout << "sending param component\n";
-    //                     param_ = dm_.param(req_.oid());
-    //                     catena::DeviceComponent_ComponentParam ans;
-    //                     param_->getParam(&ans, clientScopes_);
-                        
-    //                     // For now we are sending whole param in one go 
-    //                     status_ = CallStatus::kPostWrite;
-    //                     writer_.Write(ans, this);
-    //                 } catch (catena::exception_with_status &e) {
-    //                     status_ = CallStatus::kFinish;
-    //                     writer_.Finish(Status(static_cast<grpc::StatusCode>(e.status), e.what()), this);
-    //                 } catch (...) {
-    //                     status_ = CallStatus::kFinish;
-    //                     writer_.Finish(Status::CANCELLED, this);
-    //                 }
-    //                 break;
-
-    //             case CallStatus::kPostWrite:
-    //                 status_ = CallStatus::kFinish;
-    //                 writer_.Finish(Status::OK, this);
-    //                 break;
-
-    //             case CallStatus::kFinish:
-    //                 std::cout << "GetParam[" << objectId_ << "] finished\n";
-    //                 service->deregisterItem(this);
-    //                 break;
-    //         }
-    //     }
-
-    //   private:
-    //     CatenaServiceImpl *service_;
-    //     ServerContext context_;
-    //     std::vector<std::string> clientScopes_;
-    //     catena::GetParamPayload req_;
-    //     catena::PushUpdates res_;
-    //     ServerAsyncWriter<catena::DeviceComponent_ComponentParam> writer_;
-    //     CallStatus status_;
-    //     Device &dm_;
-    //     std::unique_ptr<catena::ParamAccessor> param_;
-    //     int objectId_;
-    //     static int objectCounter_;
-    // };
