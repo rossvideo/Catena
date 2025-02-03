f7156b8cf0267d1e50903be3e4269a848baee3a2
ZuhayrSarkerRV <ZuhayrSarkerRV@users.noreply.github.com>
2025-02-03 19:38:17 +0000

    deploy: 5900989925d46624ca3993a4894a651afe0d19d1

--- doxygen/search/functions_8.js
+++ doxygen/search/functions_8.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['has_0',['has',['../classcatena_1_1patterns_1_1Functory.html#ab8ad382d26910641abed046bb6cbc693',1,'catena::patterns::Functory']]],
-  ['hasauthz_1',['hasAuthz',['../classcatena_1_1common_1_1Authorizer.html#a888e960c8e58b0dc2674c05cf2085957',1,'catena::common::Authorizer']]],
-  ['hasmore_2',['hasMore',['../classcatena_1_1common_1_1Device_1_1DeviceSerializer.html#ae6d00e431c45be6d66a51292deacd41d',1,'catena::common::Device::DeviceSerializer']]]
+  ['hasmore_1',['hasMore',['../classcatena_1_1common_1_1Device_1_1DeviceSerializer.html#ae6d00e431c45be6d66a51292deacd41d',1,'catena::common::Device::DeviceSerializer']]]
 ];
