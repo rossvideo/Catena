564395cab69112d758786413913a6c6975e06003
BenWhitten-RV <BenWhitten-RV@users.noreply.github.com>
2025-02-10 16:30:33 +0000

    deploy: 66a94c2cf0a74935d894c9886bb248425b1b3c5d

--- doxygen/classcatena_1_1common_1_1Authorizer.js
+++ doxygen/classcatena_1_1common_1_1Authorizer.js
@@ -5,6 +5,7 @@ var classcatena_1_1common_1_1Authorizer =
     [ "Authorizer", "classcatena_1_1common_1_1Authorizer.html#a2267ed3de4f1a48fbef629a7fbb0c20e", null ],
     [ "Authorizer", "classcatena_1_1common_1_1Authorizer.html#a7d76a2fb34559e3782dc84678462e67d", null ],
     [ "~Authorizer", "classcatena_1_1common_1_1Authorizer.html#a09c54e470cc936a3fad2e2bef92ca384", null ],
+    [ "hasAuthz", "classcatena_1_1common_1_1Authorizer.html#a888e960c8e58b0dc2674c05cf2085957", null ],
     [ "operator=", "classcatena_1_1common_1_1Authorizer.html#a4280d7fb7c5cca2f2d2493d990360323", null ],
     [ "operator=", "classcatena_1_1common_1_1Authorizer.html#a4ea746b9c22dc3a0bed7a51c3d37a2d7", null ],
     [ "readAuthz", "classcatena_1_1common_1_1Authorizer.html#a2d0c5c9888480653d5c189c102513401", null ],
