7d409046f41ccf27be1c037b035245e2f37832f5
BenWhitten-RV <BenWhitten-RV@users.noreply.github.com>
2025-03-10 16:15:36 +0000

    deploy: 43233dda1bb0d0d61b08727c13a030a901573a9d

--- doxygen/classCatenaServiceImpl_1_1CallData.js
+++ doxygen/classCatenaServiceImpl_1_1CallData.js
@@ -1,5 +1,7 @@
 var classCatenaServiceImpl_1_1CallData =
 [
     [ "~CallData", "classCatenaServiceImpl_1_1CallData.html#a5275c8b9cf83c4d42685c5f2f71bf7c9", null ],
-    [ "proceed", "classCatenaServiceImpl_1_1CallData.html#aba26342ca6c03540492ec0b1d1d8e848", null ]
+    [ "getJWSToken", "classCatenaServiceImpl_1_1CallData.html#a665177806c242a75b96e89722d509cfc", null ],
+    [ "proceed", "classCatenaServiceImpl_1_1CallData.html#aba26342ca6c03540492ec0b1d1d8e848", null ],
+    [ "context_", "classCatenaServiceImpl_1_1CallData.html#a752c4dc80b04fb3bd09ddb4f648e8997", null ]
 ];
--- doxygen/search/all_15.js
+++ doxygen/search/all_15.js
@@ -6,9 +6,11 @@ var searchData=
   ['unimplemented_3',['UNIMPLEMENTED',['../namespacecatena.html#a8883f03e942ef610b66860d5b52f8d85a6f38e6fade54bc708b875c87d4ccb38f',1,'catena']]],
   ['unknown_4',['UNKNOWN',['../namespacecatena.html#a8883f03e942ef610b66860d5b52f8d85a64f74f4db3f30834d9c20a90a8dc40e9',1,'catena']]],
   ['unpop_5',['unpop',['../classcatena_1_1common_1_1Path.html#a25a93d0ba88442cb581c0c73a66a3a82',1,'catena::common::Path']]],
-  ['update_20node_20if_20required_6',['update node if required',['../md__2home_2runner_2work_2Catena_2Catena_2sdks_2cpp_2docs_2cross__compiling.html#autotoc_md7',1,'Update node if required'],['../md__2home_2runner_2work_2Catena_2Catena_2sdks_2cpp_2docs_2linux__toolchain.html#autotoc_md24',1,'Update node if required']]],
-  ['use_20the_20rest_20connection_20manager_7',['Install boost libraries if you want to use the REST connection manager',['../md__2home_2runner_2work_2Catena_2Catena_2sdks_2cpp_2docs_2linux__toolchain.html#autotoc_md25',1,'']]],
-  ['using_20docker_20with_20the_20catena_20c_20sdk_8',['Using Docker With The Catena C++ SDK',['../md__2home_2runner_2work_2Catena_2Catena_2sdks_2cpp_2docs_2docker-cpp.html',1,'']]],
-  ['using_20the_20rest_20connection_20manager_9',['Install boost if using the REST connection manager',['../md__2home_2runner_2work_2Catena_2Catena_2sdks_2cpp_2docs_2windows__toolchain.html#autotoc_md45',1,'']]],
-  ['utils_2eh_10',['utils.h',['../utils_8h.html',1,'']]]
+  ['up_20and_20running_6',['Setting up and Running',['../md__2home_2runner_2work_2Catena_2Catena_2sdks_2cpp_2docs_2docker-cpp.html#autotoc_md14',1,'']]],
+  ['up_20catena_20service_20s_20with_20docker_7',['Setting up Catena Service(s) with Docker',['../md__2home_2runner_2work_2Catena_2Catena_2sdks_2cpp_2docs_2docker-cpp.html#autotoc_md15',1,'']]],
+  ['update_20node_20if_20required_8',['update node if required',['../md__2home_2runner_2work_2Catena_2Catena_2sdks_2cpp_2docs_2cross__compiling.html#autotoc_md7',1,'Update node if required'],['../md__2home_2runner_2work_2Catena_2Catena_2sdks_2cpp_2docs_2linux__toolchain.html#autotoc_md26',1,'Update node if required']]],
+  ['use_20the_20rest_20connection_20manager_9',['Install boost libraries if you want to use the REST connection manager',['../md__2home_2runner_2work_2Catena_2Catena_2sdks_2cpp_2docs_2linux__toolchain.html#autotoc_md27',1,'']]],
+  ['using_20docker_20with_20the_20catena_20c_20sdk_10',['Using Docker With The Catena C++ SDK',['../md__2home_2runner_2work_2Catena_2Catena_2sdks_2cpp_2docs_2docker-cpp.html',1,'']]],
+  ['using_20the_20rest_20connection_20manager_11',['Install boost if using the REST connection manager',['../md__2home_2runner_2work_2Catena_2Catena_2sdks_2cpp_2docs_2windows__toolchain.html#autotoc_md47',1,'']]],
+  ['utils_2eh_12',['utils.h',['../utils_8h.html',1,'']]]
 ];
