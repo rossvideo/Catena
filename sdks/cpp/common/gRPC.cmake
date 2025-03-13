339d4f8cac9e46b2cc362f2692116eee98773b1b
John Naylor <jnaylor@rossvideo.com>
2025-03-13 09:50:16 -0500

    wip - fixing build for mac

--- sdks/cpp/common/gRPC.cmake
+++ sdks/cpp/common/gRPC.cmake
@@ -36,6 +36,7 @@ function(cmake_catena_grpc_common)
     add_library(${target} STATIC ${sources})
 
     # set up include directories
+    set (EXTERNAL_INCLUDES "/usr/local/include")
     target_include_directories(
         ${target}
         PUBLIC
@@ -43,6 +44,8 @@ function(cmake_catena_grpc_common)
             $<BUILD_INTERFACE:${GRPC_SERVICE_DIR}>
             $<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}/Catena_cpp/grpc_service>
             $<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}/Catena_cpp>
+        PRIVATE
+            ${EXTERNAL_INCLUDES}
     )
 
     # link against the gRPC interface
