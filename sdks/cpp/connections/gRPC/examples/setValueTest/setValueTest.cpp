8985f89b60bad789ab65411125d2d63257939872
BenWhitten-RV <Benjamin.Whitten@rossvideo.com>
2025-03-14 20:57:18 +0000

    Test files

---
+++ sdks/cpp/connections/gRPC/examples/setValueTest/setValueTest.cpp
@@ -0,0 +1,150 @@
+/*
+ * Copyright 2024 Ross Video Ltd
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS”
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * RE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+// device model
+#include "device.setValueTest.json.h" 
+
+//common
+#include <utils.h>
+#include <Device.h>
+#include <ParamWithValue.h>
+
+// connections/gRPC
+#include <SharedFlags.h>
+#include <ServiceImpl.h>
+#include <ServiceCredentials.h>
+
+
+#include <grpcpp/ext/proto_server_reflection_plugin.h>
+#include <grpcpp/grpcpp.h>
+#include <grpcpp/health_check_service_interface.h>
+
+
+#include "absl/flags/parse.h"
+#include "absl/flags/usage.h"
+#include "absl/strings/str_format.h"
+
+#include <iomanip>
+#include <iostream>
+#include <memory>
+#include <regex>
+#include <stdexcept>
+#include <string>
+#include <thread>
+#include <chrono>
+#include <signal.h>
+#include <functional>
+
+using grpc::Server;
+
+using namespace catena::common;
+
+
+Server *globalServer = nullptr;
+std::atomic<bool> globalLoop = true;
+
+// handle SIGINT
+void handle_signal(int sig) {
+    std::thread t([sig]() {
+        std::cout << "Caught signal " << sig << ", shutting down" << std::endl;
+        globalLoop = false;
+        if (globalServer != nullptr) {
+            globalServer->Shutdown();
+            globalServer = nullptr;
+        }
+    });
+    t.join();
+}
+
+void RunRPCServer(std::string addr)
+{
+    // install signal handlers
+    signal(SIGINT, handle_signal);
+    signal(SIGTERM, handle_signal);
+    signal(SIGKILL, handle_signal);
+
+    try {
+        // // check that static_root is a valid file path
+        // if (!std::filesystem::exists(absl::GetFlag(FLAGS_static_root))) {
+        //     std::stringstream why;
+        //     why << std::quoted(absl::GetFlag(FLAGS_static_root)) << " is not a valid file path";
+        //     throw std::invalid_argument(why.str());
+        // }
+        grpc::ServerBuilder builder;
+        // set some grpc options
+        grpc::EnableDefaultHealthCheckService(true);
+
+        builder.AddListeningPort(addr, catena::getServerCredentials());
+        std::unique_ptr<grpc::ServerCompletionQueue> cq = builder.AddCompletionQueue();
+        std::string EOPath = absl::GetFlag(FLAGS_static_root);
+        bool authz = absl::GetFlag(FLAGS_authz);
+        uint32_t defaultMaxArraySize = absl::GetFlag(FLAGS_default_max_array_size);
+        uint32_t defaultTotalArraySize = absl::GetFlag(FLAGS_default_total_array_size);
+        dm.set_default_max_length(defaultMaxArraySize);
+        dm.set_default_total_length(defaultTotalArraySize);
+        CatenaServiceImpl service(cq.get(), dm, EOPath, authz);
+
+        // Updating device's default max array length.
+        dm.set_default_max_length(absl::GetFlag(FLAGS_default_max_array_size));
+
+        builder.RegisterService(&service);
+
+
+        std::unique_ptr<Server> server(builder.BuildAndStart());
+        std::cout << "GRPC on " << addr << " secure mode: " << absl::GetFlag(FLAGS_secure_comms) << '\n';
+
+        globalServer = server.get();
+
+        service.init();
+        std::thread cq_thread([&]() { service.processEvents(); });
+
+        // wait for the server to shutdown and tidy up
+        server->Wait();
+
+        cq->Shutdown();
+        cq_thread.join();
+
+    } catch (std::exception &why) {
+        std::cerr << "Problem: " << why.what() << '\n';
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    std::string addr;
+    absl::SetProgramUsageMessage("Runs the Catena Service");
+    absl::ParseCommandLine(argc, argv);
+  
+    addr = absl::StrFormat("0.0.0.0:%d", absl::GetFlag(FLAGS_port));
+  
+    std::thread catenaRpcThread(RunRPCServer, addr);
+    catenaRpcThread.join();
+    return 0;
+}
