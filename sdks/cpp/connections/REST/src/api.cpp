c571d353769b10f690316a7eea9766a2b7455c80
BenWhitten-RV <Benjamin.Whitten@rossvideo.com>
2025-03-24 20:54:57 +0000

    Implemented DeviceRequest with streaming. Bit of a mess right now. Switched to boost::asio, will have to convert other previously completed functions

--- sdks/cpp/connections/REST/src/api.cpp
+++ sdks/cpp/connections/REST/src/api.cpp
@@ -55,21 +55,14 @@ void expandEnvVariables(std::string &str) {
     }
 }
 
-API::API(Device &dm, uint16_t port) : version_{"1.0.0"}, port_{port}, dm_{dm} {
+API::API(Device &dm, uint16_t port) : version_{"1.0.0"}, port_{port}, dm_{dm},
+    acceptor_{io_context_, Tcp::endpoint(Tcp::v4(), port)} {
     // Flag does not really work at the moment :/
-    authorizationEnabled_ = absl::GetFlag(FLAGS_authz);
+    // authorizationEnabled_ = absl::GetFlag(FLAGS_authz);
+    authorizationEnabled_ = true;
     if (authorizationEnabled_) {
         std::cerr<<"Authorization enabled"<<std::endl;
     }
-
-    CROW_ROUTE(app_, "/v1/GetPopulatedSlots")
-    ([this]() { return this->getPopulatedSlots(); });
-    CROW_ROUTE(app_, "/v1/GetValue/slot/<int>/oid/<path>")
-    ([this](const crow::request& req, int slot, std::string oid) { return this->getValue(req, slot, oid); });
-    CROW_ROUTE(app_, "/v1/SetValue").methods(crow::HTTPMethod::PUT)
-    ([this](const crow::request& req) { return this->setValue(req); });
-    CROW_ROUTE(app_, "/v1/MultiSetValue").methods(crow::HTTPMethod::PUT)
-    ([this](const crow::request& req) { return this->multiSetValue(req); });
 }
 
 std::string API::version() const {
@@ -77,29 +70,49 @@ std::string API::version() const {
 }
 
 void API::run() {
-    // if (is_port_in_use_()) {
-    //     std::cerr << "Port " << port_ << " is already in use" << std::endl;
-    //     return;
-    // }
-    // asio::ssl::context ssl_context(asio::ssl::context::tlsv12);
+    // TLS handled by Envoyproxy
+    while (true) {
+        // Waiting for a connection.
+        Tcp::socket socket(io_context_);
+        acceptor_.accept(socket);
 
-    // // find our certs
-    // std::string path_to_certs(absl::GetFlag(FLAGS_certs));
-    // expandEnvVariables(path_to_certs);
-    
-    // path_to_certs = "/home/vboxuser/test_certs";
+        // When a connection has been made, detatch to handle asynchronously.
+        std::thread([this, socket = std::move(socket)]() mutable {
+            // Reading the request.
+            boost::asio::streambuf buffer;
+            boost::asio::read_until(socket, buffer, "\r\n\r\n");
+            std::istream request_stream(&buffer);
+            std::string header;
+            std::getline(request_stream, header);
+            // Extracting method and request
+            std::string method = header.substr(0, header.find(" "));
+            std::string request = header.substr(header.find("/"));
 
-    // // Set up SSL/TLS
-    // ssl_context.set_options(asio::ssl::context::default_workarounds | 
-    //                         asio::ssl::context::no_sslv2 | 
-    //                         asio::ssl::context::single_dh_use);
+            // Setting up authorizer if enabled.
+            std::shared_ptr<catena::common::Authorizer> sharedAuthz;
+            catena::common::Authorizer* authz = nullptr;
+            if (authorizationEnabled_) {
+                while(std::getline(request_stream, header) && header != "\r") {
+                    try {
+                        if (header.starts_with("Authorization: Bearer ")) {
+                            std::string jwsToken = header.substr(std::string("Authorization: Bearer ").length());
+                            jwsToken.erase(jwsToken.length() - 1); // rm newline
+                            sharedAuthz = std::make_shared<catena::common::Authorizer>(jwsToken);
+                            authz = sharedAuthz.get();
+                            break;
+                        }
+                    } catch (catena::exception_with_status& err) {
+                        // TODO later
+                    }
+                }
+            } else {
+                authz = &catena::common::Authorizer::kAuthzDisabled;
+            }
 
-    // // Load certificate and private key files
-    // ssl_context.use_certificate_chain_file(path_to_certs + "/server.crt");
-    // ssl_context.use_private_key_file(path_to_certs + "/server.key", asio::ssl::context::pem);
-    // ssl_context.load_verify_file(path_to_certs + "/ca.crt");
-    app_.port(port_).run();
-    // app_.port(port_).ssl(std::move(ssl_context)).run();
+            // Routing the request based on the name.
+            route(method, request, socket, authz);
+        }).detach();
+    }
 }
 
 std::string API::getJWSToken(const crow::request& req) const {
@@ -109,6 +122,18 @@ std::string API::getJWSToken(const crow::request& req) const {
     }
     // Getting token (after "bearer") and returning as an std::string.
     return auth_header.substr(std::string("Bearer ").length());
+}
+
+std::string API::getField(std::string& request, std::string field) const {
+    std::string foundField = "";
+    field = "/" + field + "/";
+    std::size_t pos = request.find(field) + field.length();
+    // Extracts the value between field and the next "/"
+    if (pos != std::string::npos) {
+        foundField = request.substr(pos);
+        foundField = foundField.substr(0, foundField.find("/"));
+    }
+    return foundField;
 }
 
 crow::response API::finish(google::protobuf::Message& msg) const {
@@ -130,6 +155,22 @@ crow::response API::finish(google::protobuf::Message& msg) const {
         res.write(json_output);
     }
     return res;
+}
+
+void API::write(Tcp::socket& socket, google::protobuf::Message& msg) const {   
+    // Converting the value to JSON.
+    std::string json_output;
+    google::protobuf::util::JsonPrintOptions options;
+    options.add_whitespace = true;
+    auto status = MessageToJsonString(msg, &json_output, options);
+
+    // Check if the conversion was successful.
+    if (!status.ok()) {
+        throw catena::exception_with_status("Failed to convert protobuf to JSON", catena::StatusCode::INVALID_ARGUMENT);
+    } else {
+        std::string chunk_size = std::format("{:x}", json_output.size());
+        boost::asio::write(socket, boost::asio::buffer(chunk_size + "\r\n" + json_output + "\r\n"));
+    }
 }
 
 bool API::is_port_in_use_() const {
