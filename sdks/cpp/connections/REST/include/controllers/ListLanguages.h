024c1c078f4bd091347d0c0df236096b24aeddcb
BenWhitten-RV <benjamin.whitten@rossvideo.com>
2025-04-10 19:42:15 +0000

    Implemented listlanguages

---
+++ sdks/cpp/connections/REST/include/controllers/ListLanguages.h
@@ -0,0 +1,143 @@
+/*
+ * Copyright 2025 Ross Video Ltd
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * RE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * @file ListLanguagess.h
+ * @brief Implements REST ListLanguages RPC.
+ * @author benjamin.whitten@rossvideo.com
+ * @copyright Copyright © 2025 Ross Video Ltd
+ */
+
+#pragma once
+
+// protobuf
+#include <interface/device.pb.h>
+#include <google/protobuf/util/json_util.h>
+ 
+// common
+#include <rpc/TimeNow.h>
+#include <Status.h>
+#include <Device.h>
+#include <utils.h>
+#include <Authorization.h>
+#include <Enums.h>
+
+// Connections/REST
+#include "SocketReader.h"
+#include "SocketWriter.h"
+#include "interface/ICallData.h"
+
+namespace catena {
+namespace REST {
+
+/**
+ * @brief ICallData class for the ListLanguages REST RPC.
+ */
+class ListLanguages : public ICallData {
+  public:
+    // Specifying which Device and IParam to use (defaults to catena::...)
+    using Device = catena::common::Device;
+    using IParam = catena::common::IParam;
+
+    /**
+     * @brief Constructor for the ListLanguages RPC. Calls proceed() once
+     * initialized.
+     *
+     * @param socket The socket to write the response to.
+     * @param context The SocketReader object.
+     * @param dm The device to get the value from.
+     */ 
+    ListLanguages(tcp::socket& socket, SocketReader& context, Device& dm);
+    /**
+     * @brief ListLanguages's main process.
+     */
+    void proceed() override;
+    /**
+     * @brief Finishes the ListLanguages process.
+     */
+    void finish() override;
+    /**
+     * @brief Creates a new rpc object for use with GenericFactory.
+     * 
+     * @param socket The socket to write the response stream to.
+     * @param context The SocketReader object.
+     * @param dm The device to connect to.
+     */
+    static ICallData* makeOne(tcp::socket& socket, SocketReader& context, Device& dm) {
+      return new ListLanguages(socket, context, dm);
+    }
+  private:
+    /**
+     * @brief Helper function to write status messages to the API console.
+     * 
+     * @param status The current state of the RPC (kCreate, kFinish, etc.)
+     * @param ok The status of the RPC (open or closed).
+     */
+    inline void writeConsole(CallStatus status, bool ok) const override {
+      std::cout << "ListLanguages::proceed[" << objectId_ << "]: "
+                << catena::common::timeNow() << " status: "
+                << static_cast<int>(status) <<", ok: "<< std::boolalpha << ok
+                << std::endl;
+    }
+
+    /**
+     * @brief The socket to write the response to.
+     */
+    tcp::socket& socket_;
+    /**
+     * @brief The SocketWriter object for writing to socket_.
+     */
+    SocketWriter writer_;
+    /**
+     * @brief The device to set values of.
+     */
+    Device& dm_;
+    /**
+     * @brief Flag indicating if the RPC is working correctly.
+     */
+    bool ok_;
+
+    /**
+     * @brief The slot of the device to get the value from.
+     */
+    int slot_;
+
+    /**
+     * @brief ID of the ListLanguages object
+     */
+    int objectId_;
+    /**
+     * @brief The total # of ListLanguages objects.
+     */
+    static int objectCounter_;
+};
+
+}; // namespace REST
+}; // namespace catena
--- sdks/cpp/connections/REST/src/ServiceImpl.cpp
+++ sdks/cpp/connections/REST/src/ServiceImpl.cpp
@@ -10,6 +10,7 @@ using catena::REST::CatenaServiceImpl;
 #include <controllers/DeviceRequest.h>
 #include <controllers/GetValue.h>
 #include <controllers/GetPopulatedSlots.h>
+#include <controllers/ListLanguages.h>
 using catena::REST::Connect;
 
 #include "absl/flags/flag.h"
@@ -45,6 +46,7 @@ CatenaServiceImpl::CatenaServiceImpl(Device &dm, std::string& EOPath, bool authz
     router_.addProduct("GET/v1/GetValue",          GetValue::makeOne);
     router_.addProduct("PUT/v1/MultiSetValue",     MultiSetValue::makeOne);
     router_.addProduct("PUT/v1/SetValue",          SetValue::makeOne);
+    router_.addProduct("GET/v1/ListLanguages",     ListLanguages::makeOne);
 }
 
 // Initializing the shutdown signal for all open connections.
