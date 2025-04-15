da32e20c5a6b10b188e0d672ceb230baf6a028a5
BenWhitten-RV <benjamin.whitten@rossvideo.com>
2025-04-15 14:58:20 +0000

    More tests and cleanup

--- sdks/cpp/connections/REST/tests/SocketReader_test.cpp
+++ sdks/cpp/connections/REST/tests/SocketReader_test.cpp
@@ -1,37 +1,40 @@
-//  Copyright 2025 Ross Video Ltd
-//  
-//  Redistribution and use in source and binary forms, with or without
-//  modification, are permitted provided that the following conditions are met:
-//  
-//  1. Redistributions of source code must retain the above copyright notice,
-//  this list of conditions and the following disclaimer.
-//  
-//  2. Redistributions in binary form must reproduce the above copyright notice,
-//  this list of conditions and the following disclaimer in the documentation
-//  and/or other materials provided with the distribution.
-//  
-//  3. Neither the name of the copyright holder nor the names of its
-//  contributors may be used to endorse or promote products derived from this
-//  software without specific prior written permission.
-//  
-//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-//  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-//  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-//  RE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-//  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-//  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-//  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-//  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-//  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-//  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-//  POSSIBILITY OF SUCH DAMAGE.
-// 
-// 
-//  this file is for testing the Device.cpp file
-//
-//  Author: benjamin.whitten@rossvideo.com
-//  Date: 25/04/11
-// 
+/*
+ * Copyright 2025 Ross Video Ltd
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS”
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * RE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * @brief This file is for testing the SocketWriter.cpp file.
+ * @author benjamin.whitten@rossvideo.com
+ * @date 25/04/15
+ * @copyright Copyright © 2025 Ross Video Ltd
+ */
+
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 #include <filesystem>
@@ -55,90 +58,74 @@ namespace fs = std::filesystem;
 //         MOCK_METHOD(bool, authorizationEnabled, (), (const, override));
 // };
 
-// Helper function which writes a fake request to the client socket.
-void mockRequest(tcp::socket& client_socket, boost::asio::io_context& io_context) {
+// Writes a request to and returns server socket for SocketReader tests.
+std::unique_ptr<tcp::socket> mockServerSocket() {
+    boost::asio::io_context io_context;
+    tcp::socket serverSocket(io_context);
+    tcp::socket clientSocket(io_context);
     tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 0));
-    tcp::socket server_socket(io_context);
-    std::thread server_thread([&]() {
-        // Accepting client connection and writing a fake GET request.
-        acceptor.accept(server_socket);
-        std::string request = "GET localhost:443/v1/GetValue/slot/1/oid/text_box HTTP/1.1\n"
-                              "Origin: test_origin\n"
-                              "User-Agent: test_agent\n"
-                              "Authorization: Bearer test_bearer \n"
-                              "Content-Length: 15\n"
-                              "\r\n"
-                              "{\n"
-                              "  test_body\n"
-                              "}\n"
-                              "\r\n\r\n";
-        boost::asio::write(server_socket, boost::asio::buffer(request));
-    });
-    // Connecting the client socket to the acceptor and waiting for response.
-    client_socket.connect(acceptor.local_endpoint());
-    server_thread.join();
+    // Linking client and server sockets.
+    serverSocket.connect(acceptor.local_endpoint());
+    acceptor.accept(clientSocket);
+    // Writing request to server.
+    std::string request = "GET localhost:443/v1/GetValue/slot/1/oid/text_box HTTP/1.1\n"
+                          "Origin: test_origin\n"
+                          "User-Agent: test_agent\n"
+                          "Authorization: Bearer test_bearer \n"
+                          "Content-Length: 15\n"
+                          "\r\n"
+                          "{\n"
+                          "  test_body\n"
+                          "}\n"
+                          "\r\n\r\n";
+    boost::asio::write(clientSocket, boost::asio::buffer(request));
+
+    // Returning the serverSocket.
+    return std::make_unique<tcp::socket>(std::move(serverSocket));
 }
 
 // Testing SocketReader with authorization disabled.
 TEST(REST_API_tests, SocketReader_NormalCase) {
-    // Creating a client socket and sending a fake GET request to it.
-    boost::asio::io_context io_context;
-    tcp::socket client_socket(io_context);
-    mockRequest(client_socket, io_context);
+    auto serverSocket = mockServerSocket();
     // Creating a SocketReader and reading the request.
-    catena::REST::SocketReader socket_reader;
-    socket_reader.read(client_socket, false);
-    // Getting answers from the SocketReader.
-    std::string method = socket_reader.method();
-    std::string call = socket_reader.rpc();
+    catena::REST::SocketReader socketReader;
+    socketReader.read(*serverSocket, false);
+    // Reading extracted fields from the socketReader.
     std::unordered_map<std::string, std::string> fields = {
         {"oid", ""},
         {"slot", ""}
     };
-    socket_reader.fields(fields);
-    bool authz = socket_reader.authorizationEnabled();
-    std::string token = socket_reader.jwsToken();
-    std::string origin = socket_reader.origin();
-    std::string jsonBody = socket_reader.jsonBody();
-    // Checking answer.
-    EXPECT_EQ(method, "GET");
-    EXPECT_EQ(call, "/v1/GetValue");
-    EXPECT_EQ(fields.at("slot"), "1");
-    EXPECT_EQ(fields.at("oid"), "text_box");
-    EXPECT_EQ(authz, false);
-    EXPECT_EQ(token, "");
-    EXPECT_EQ(origin, "test_origin");
-    EXPECT_EQ(jsonBody, "{\n  test_body\n}");
+    socketReader.fields(fields);
+    // Checking answers.
+    EXPECT_EQ(socketReader.method(),               "GET"              );
+    EXPECT_EQ(socketReader.rpc(),                  "/v1/GetValue"     );
+    EXPECT_EQ(fields.at("slot"),                   "1"                );
+    EXPECT_EQ(fields.at("oid"),                    "text_box"         );
+    EXPECT_EQ(socketReader.authorizationEnabled(), false              );
+    EXPECT_EQ(socketReader.jwsToken(),             ""                 );
+    EXPECT_EQ(socketReader.origin(),               "test_origin"      );
+    EXPECT_EQ(socketReader.jsonBody(),             "{\n  test_body\n}");
 }
 
 // Testing SocketReader with authorization enabled.
 TEST(REST_API_tests, SocketReader_AuthzCase) {
-    // Creating a client socket and sending a fake GET request to it.
-    boost::asio::io_context io_context;
-    tcp::socket client_socket(io_context);
-    mockRequest(client_socket, io_context);
+    auto serverSocket = mockServerSocket();
     // Creating a SocketReader and reading the request.
-    catena::REST::SocketReader socket_reader;
-    socket_reader.read(client_socket, true);
-    // Getting answers from the SocketReader.
-    std::string method = socket_reader.method();
-    std::string call = socket_reader.rpc();
+    catena::REST::SocketReader socketReader;
+    socketReader.read(*serverSocket, true);
+    // Getting fields from the SocketReader.
     std::unordered_map<std::string, std::string> fields = {
         {"oid", ""},
         {"slot", ""}
     };
-    socket_reader.fields(fields);
-    bool authz = socket_reader.authorizationEnabled();
-    std::string token = socket_reader.jwsToken();
-    std::string origin = socket_reader.origin();
-    std::string jsonBody = socket_reader.jsonBody();
+    socketReader.fields(fields);
     // Checking answer.
-    EXPECT_EQ(method, "GET");
-    EXPECT_EQ(call, "/v1/GetValue");
-    EXPECT_EQ(fields.at("slot"), "1");
-    EXPECT_EQ(fields.at("oid"), "text_box");
-    EXPECT_EQ(authz, true);
-    EXPECT_EQ(token, "test_bearer");
-    EXPECT_EQ(origin, "test_origin");
-    EXPECT_EQ(jsonBody, "{\n  test_body\n}");
+    EXPECT_EQ(socketReader.method(),               "GET"              );
+    EXPECT_EQ(socketReader.rpc(),                  "/v1/GetValue"     );
+    EXPECT_EQ(fields.at("slot"),                   "1"                );
+    EXPECT_EQ(fields.at("oid"),                    "text_box"         );
+    EXPECT_EQ(socketReader.authorizationEnabled(), true               );
+    EXPECT_EQ(socketReader.jwsToken(),             "test_bearer"      );
+    EXPECT_EQ(socketReader.origin(),               "test_origin"      );
+    EXPECT_EQ(socketReader.jsonBody(),             "{\n  test_body\n}");
 }
--- sdks/cpp/connections/REST/tests/SocketWriter_test.cpp
+++ sdks/cpp/connections/REST/tests/SocketWriter_test.cpp
@@ -1,37 +1,40 @@
-//  Copyright 2025 Ross Video Ltd
-//  
-//  Redistribution and use in source and binary forms, with or without
-//  modification, are permitted provided that the following conditions are met:
-//  
-//  1. Redistributions of source code must retain the above copyright notice,
-//  this list of conditions and the following disclaimer.
-//  
-//  2. Redistributions in binary form must reproduce the above copyright notice,
-//  this list of conditions and the following disclaimer in the documentation
-//  and/or other materials provided with the distribution.
-//  
-//  3. Neither the name of the copyright holder nor the names of its
-//  contributors may be used to endorse or promote products derived from this
-//  software without specific prior written permission.
-//  
-//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-//  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-//  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-//  RE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-//  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-//  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-//  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-//  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-//  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-//  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-//  POSSIBILITY OF SUCH DAMAGE.
-// 
-// 
-//  this file is for testing the Device.cpp file
-//
-//  Author: benjamin.whitten@rossvideo.com
-//  Date: 25/04/11
-// 
+/*
+ * Copyright 2025 Ross Video Ltd
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS”
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * RE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ * @brief This file is for testing the SocketWriter.cpp file.
+ * @author benjamin.whitten@rossvideo.com
+ * @date 25/04/15
+ * @copyright Copyright © 2025 Ross Video Ltd
+ */
+
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 #include <filesystem>
@@ -39,7 +42,9 @@
 #include <string>
 #include <tuple>
 
+// protobuf
 #include <interface/device.pb.h>
+#include <google/protobuf/util/json_util.h>
 
 #include <Status.h>
 
@@ -47,58 +52,126 @@ namespace fs = std::filesystem;
 
 #include "SocketWriter.h"
 
+// Converts entire socket response to string.
+std::string read(tcp::socket& socket) {
+    boost::asio::streambuf buffer;
+    boost::asio::read_until(socket, buffer, "\r\n\r\n");
+    std::istream response_stream(&buffer);
+    return std::string(std::istreambuf_iterator<char>(response_stream), std::istreambuf_iterator<char>());
+}
+
+/*
+ * Generates what the answer for SocketWriter should look like depending on
+ * status and jsonBody.
+ */
+inline std::string SocketWriterAns(const std::string& jsonBody) {
+    return "HTTP/1.1 200 OK\r\n"
+           "Content-Type: application/json\r\n"
+           "Content-Length: " + std::to_string(jsonBody.length()) + "\r\n"
+           "Access-Control-Allow-Origin: *\r\n"
+           "Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS\r\n"
+           "Access-Control-Allow-Headers: Content-Type, Authorization, accept, Origin, X-Requested-With\r\n"
+           "Access-Control-Allow-Credentials: true\r\n"
+           "Connection: close\r\n"
+           "\r\n" +
+           jsonBody;
+}
+
+// Testing writing one msg with SocketWriter.
 TEST(REST_API_tests, SocketWriter_NormalWrite) {
-    // Variables.
     boost::asio::io_context io_context;
     tcp::socket client_socket(io_context);
     tcp::socket server_socket(io_context);
     tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 0));
-    // Connecting client and server sockets and writing message.
+    // Linking client and server sockets.
     client_socket.connect(acceptor.local_endpoint());
     acceptor.accept(server_socket);
-    catena::Empty ans = catena::Empty();
+    // Creating server socket writer and writing message.
     catena::REST::SocketWriter writer(server_socket);
-    writer.finish(ans);
-    // Reading and checking the response from the server socket.
-    boost::asio::streambuf buffer;
-    boost::asio::read_until(client_socket, buffer, "\r\n\r\n");
-    std::istream response_stream(&buffer);
-    std::string response;
-    std::getline(response_stream, response);
-    EXPECT_EQ(response, "HTTP/1.1 200 OK\r");
-    // Cycle to json body
-    while(std::getline(response_stream, response) && response != "\r") {}
-    // Checking the json body.
-    std::string jsonBody;
-    std::getline(response_stream, jsonBody);
-    EXPECT_EQ(jsonBody, "{}");
+    catena::Empty msg = catena::Empty();
+    writer.finish(msg);
+    // Reading and checking the response from the client socket.
+    EXPECT_EQ(read(client_socket), SocketWriterAns("{}\n"));
 }
 
-TEST(REST_API_tests, SocketWriter_WriteError) {
-    // Variables.
+// Testing writing errors with SocketWriter.
+TEST(REST_API_tests, SocketWriter_WriteError) {   
     boost::asio::io_context io_context;
     tcp::socket client_socket(io_context);
     tcp::socket server_socket(io_context);
     tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 0));
-    // Connecting client and server sockets and writing message.
+
+    // Linking client and server sockets.
     client_socket.connect(acceptor.local_endpoint());
     acceptor.accept(server_socket);
-    catena::exception_with_status error("test_error", catena::StatusCode::INVALID_ARGUMENT);
+
+    // Creating server socket writer and writing error message.
     catena::REST::SocketWriter writer(server_socket);
-    writer.write(error);
-    // Reading and checking the response from the server socket.
-    boost::asio::streambuf buffer;
-    boost::asio::read_until(client_socket, buffer, "\r\n\r\n");
-    std::istream response_stream(&buffer);
-    std::string response;
-    std::getline(response_stream, response);
-    EXPECT_EQ(response, "HTTP/1.1 406 test_error\r");
+    catena::exception_with_status msg("test_error", catena::StatusCode::INVALID_ARGUMENT);
+    writer.write(msg);
+
+    // Expected answer
+    std::string ans = "HTTP/1.1 406 test_error\r\n"
+                      "Content-Type: text/plain\r\n"
+                      "Content-Length: 10\r\n"
+                      "Access-Control-Allow-Origin: *\r\n"
+                      "Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS\r\n"
+                      "Access-Control-Allow-Headers: Content-Type, Authorization, accept, Origin, X-Requested-With\r\n"
+                      "Access-Control-Allow-Credentials: true\r\n"
+                      "Connection: close\r\n"
+                      "\r\n"
+                      "test_error";    
+
+    // Reading and checking the response from the client socket.
+    EXPECT_EQ(read(client_socket), ans);
 }
 
-TEST(REST_API_tests, SocketWriter_WriteMultiple) {}
+// Testing writing muliple buffered messages with SocketWriter.
+TEST(REST_API_tests, SocketWriter_WriteMultiple) {
+    boost::asio::io_context io_context;
+    tcp::socket client_socket(io_context);
+    tcp::socket server_socket(io_context);
+    tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), 0));
+    // Linking client and server sockets.
+    client_socket.connect(acceptor.local_endpoint());
+    acceptor.accept(server_socket);
+    
+    // Creating server socket writer and writing messages.
+    catena::REST::SocketWriter writer(server_socket);
+    std::vector<std::string> msgs = {
+        "{\n \"stringValue\": \"Test string #1\"\n}",
+        "{\n \"float32Value\": 2\n}",
+        "{\n \"stringValue\": \"Test string #3\"\n}",
+        "{\n \"stringValue\": \"Test string #4\"\n}",
+        "{\n \"int32Value\": 5\n}",
+    };
+    for (std::string msgJson : msgs) {
+        catena::Value msg;
+        auto status = google::protobuf::util::JsonStringToMessage(absl::string_view(msgJson), &msg);
+        writer.write(msg);
+    }
+    writer.finish();
 
+    // Generating the expected response json body from msgs.
+    std::string resBody = "{\n\"response\": [\n" + msgs[0];
+    for (int i = 1; i < msgs.size(); i += 1) {
+            resBody += ",\n" + msgs[i];
+    }
+    resBody += "\n]\n}";
+
+    // Reading and checking the response from the server socket.
+    EXPECT_EQ(read(client_socket), SocketWriterAns(resBody));
+}
+
+/**
+ * @todo Below we are waiting on SSEWriter to be merged with dev.
+ */
+
+// Testing writing a number of messages with SSEWriter.
 TEST(REST_API_tests, SSEWriter_NormalWrite) {}
 
+// Testing writing a number of errors with SSEWriter.
 TEST(REST_API_tests, SSEWriter_WriteError) {}
 
-TEST(REST_API_tests, SSEWriter_WriteMultiple) {}
+// Testing writing a mix of messages and errors with SSEWriter.
+TEST(REST_API_tests, SSEWriter_WriteMix) {}
