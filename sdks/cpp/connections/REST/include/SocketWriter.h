af6b16a6a20194f805db87d90c3b5be0a68329b5
BenWhitten-RV <Benjamin.whitten@rossvideo.com>
2025-04-25 19:00:19 +0000

    updated response code map

--- sdks/cpp/connections/REST/include/SocketWriter.h
+++ sdks/cpp/connections/REST/include/SocketWriter.h
@@ -163,23 +163,23 @@ class SSEWriter : public ISocketWriter {
  */
 const std::map<catena::StatusCode, int> codeMap_ {
   {catena::StatusCode::OK,                  200},
-  {catena::StatusCode::CANCELLED,           410},
-  {catena::StatusCode::UNKNOWN,             404},
-  {catena::StatusCode::INVALID_ARGUMENT,    406},
-  {catena::StatusCode::DEADLINE_EXCEEDED,   408},
-  {catena::StatusCode::NOT_FOUND,           410},
+  {catena::StatusCode::CANCELLED,           499},
+  {catena::StatusCode::UNKNOWN,             500},
+  {catena::StatusCode::INVALID_ARGUMENT,    400},
+  {catena::StatusCode::DEADLINE_EXCEEDED,   504},
+  {catena::StatusCode::NOT_FOUND,           404},
   {catena::StatusCode::ALREADY_EXISTS,      409},
-  {catena::StatusCode::PERMISSION_DENIED,   401},
-  {catena::StatusCode::UNAUTHENTICATED,     407},
-  {catena::StatusCode::RESOURCE_EXHAUSTED,  8},   // TODO
+  {catena::StatusCode::PERMISSION_DENIED,   403},
+  {catena::StatusCode::UNAUTHENTICATED,     401},
+  {catena::StatusCode::RESOURCE_EXHAUSTED,  429},
   {catena::StatusCode::FAILED_PRECONDITION, 412},
-  {catena::StatusCode::ABORTED,             10},  // TODO
-  {catena::StatusCode::OUT_OF_RANGE,        416},
+  {catena::StatusCode::ABORTED,             409},
+  {catena::StatusCode::OUT_OF_RANGE,        400},
   {catena::StatusCode::UNIMPLEMENTED,       501},
   {catena::StatusCode::INTERNAL,            500},
   {catena::StatusCode::UNAVAILABLE,         503},
-  {catena::StatusCode::DATA_LOSS,           15},  // TODO
-  {catena::StatusCode::DO_NOT_USE,          -1},  // TODO
+  {catena::StatusCode::DATA_LOSS,           500},
+  {catena::StatusCode::DO_NOT_USE,          500},
 };
 
 }; // Namespace REST
