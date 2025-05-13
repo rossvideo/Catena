352a2290e99e3160eb7a62712199dbb62c7ff2a9
BenWhitten-RV <Benjamin.whitten@rossvideo.com>
2025-05-13 15:54:21 +0000

    AddLanguage tests

--- sdks/cpp/connections/REST/tests/AddLanguage_test.cpp
+++ sdks/cpp/connections/REST/tests/AddLanguage_test.cpp
@@ -29,7 +29,7 @@
  */
 
 /**
- * @brief This file is for testing the LanguagePackRequest.cpp file.
+ * @brief This file is for testing the AddLanguage.cpp file.
  * @author benjamin.whitten@rossvideo.com
  * @date 25/05/13
  * @copyright Copyright © 2025 Ross Video Ltd
@@ -51,138 +51,315 @@
 #include "MockClasses.h"
 
 // REST
-#include "controllers/LanguagePackRequest.h"
+#include "controllers/AddLanguage.h"
 #include "SocketWriter.h"
 
 using namespace catena::common;
 using namespace catena::REST;
 
 // Fixture
-class RESTLanguagePackRequestTests : public ::testing::Test, public SocketHelper {
+class RESTAddLanguageTests : public ::testing::Test, public SocketHelper {
   protected:
-    RESTLanguagePackRequestTests() : SocketHelper(&serverSocket, &clientSocket) {}
+    RESTAddLanguageTests() : SocketHelper(&serverSocket, &clientSocket) {}
 
     void SetUp() override {
         // Redirecting cout to a stringstream for testing.
         oldCout = std::cout.rdbuf(MockConsole.rdbuf());
 
-        // Creating LanguagePackRequest object.
+        // Creating AddLanguage object.
         EXPECT_CALL(context, origin()).Times(1).WillOnce(::testing::ReturnRef(origin));
-        languagePackRequest = LanguagePackRequest::makeOne(serverSocket, context, dm);
+        addLanguage = AddLanguage::makeOne(serverSocket, context, dm);
     }
 
     void TearDown() override {
         std::cout.rdbuf(oldCout); // Restoring cout
         // Cleanup code here
-        if (languagePackRequest) {
-            delete languagePackRequest;
+        if (addLanguage) {
+            delete addLanguage;
         }
     }
+
+    // A collection of calls expected in every proceed test save for when json
+    // fails to parse.
+    void expectedProceedCalls() {
+        EXPECT_CALL(context, slot()).Times(1).WillOnce(::testing::Return(1));
+        EXPECT_CALL(context, fields("id")).Times(1).WillOnce(::testing::ReturnRef(language));
+        EXPECT_CALL(context, jsonBody()).Times(1).WillOnce(::testing::ReturnRef(jsonBody));
+        EXPECT_CALL(context, authorizationEnabled()).Times(1).WillOnce(::testing::Return(authz));
+        if (authz) {
+            EXPECT_CALL(context, jwsToken()).Times(1).WillOnce(::testing::ReturnRef(mockToken));
+        }
+    }
+
     std::stringstream MockConsole;
     std::streambuf* oldCout;
 
-    std::string language = "en";
-
     MockSocketReader context;
     MockDevice dm;
+    catena::REST::ICallData* addLanguage = nullptr;
+
+    // Test Values
+    /* Authz just tests for a properly encrypted token, proxy handles authz.
+     * This is a random RSA token I made jwt.io it is not a security risk I
+     * swear. */
+    std::string mockToken = "eyJhbGciOiJSUzI1NiIsInR5cCI6ImF0K2p3dCJ9.eyJzdWIi"
+                            "OiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwic2Nvc"
+                            "GUiOiJzdDIxMzg6bW9uOncgc3QyMTM4Om9wOncgc3QyMTM4Om"
+                            "NmZzp3IHN0MjEzODphZG06dyIsImlhdCI6MTUxNjIzOTAyMiw"
+                            "ibmJmIjoxNzQwMDAwMDAwLCJleHAiOjE3NTAwMDAwMDB9.dTo"
+                            "krEPi_kyety6KCsfJdqHMbYkFljL0KUkokutXg4HN288Ko965"
+                            "3v0khyUT4UKeOMGJsitMaSS0uLf_Zc-JaVMDJzR-0k7jjkiKH"
+                            "kWi4P3-CYWrwe-g6b4-a33Q0k6tSGI1hGf2bA9cRYr-VyQ_T3"
+                            "RQyHgGb8vSsOql8hRfwqgvcldHIXjfT5wEmuIwNOVM3EcVEaL"
+                            "yISFj8L4IDNiarVD6b1x8OXrL4vrGvzesaCeRwP8bxg4zlg_w"
+                            "bOSA8JaupX9NvB4qssZpyp_20uHGh8h_VC10R0k9NKHURjs9M"
+                            "dvJH-cx1s146M27UmngWUCWH6dWHaT2au9en2zSFrcWHw";
+    std::string language = "en";
     std::mutex mockMutex;
-    catena::REST::ICallData* languagePackRequest = nullptr;
+    std::string jsonBody = "{\"name\":\"English\",\"words\":{\"greeting\":\"Hello\",\"parting\":\"Goodbye\"}}";
+    bool authz = false;
 };
 
 /*
  * ============================================================================
- *                               LanguagePackRequest tests
+ *                               AddLanguage tests
  * ============================================================================
  * 
- * TEST 1 - Creating a LanguagePackRequest object with makeOne.
+ * TEST 1 - Creating a AddLanguage object with makeOne.
  */
-TEST_F(RESTLanguagePackRequestTests, LanguagePackRequest_create) {
-    // Making sure languagePackRequest is created from the SetUp step.
-    ASSERT_TRUE(languagePackRequest);
+TEST_F(RESTAddLanguageTests, AddLanguage_create) {
+    // Making sure addLanguage is created from the SetUp step.
+    ASSERT_TRUE(addLanguage);
 }
 
 /* 
- * TEST 2 - Normal case for LanguagePackRequest proceed().
+ * TEST 2 - Normal case for AddLanguage proceed() without authz.
  */
-TEST_F(RESTLanguagePackRequestTests, ListLanguages_proceedNormal) {
+TEST_F(RESTAddLanguageTests, AddLanguage_proceedNormal) {
     // Setting up the returnVal to test with.
-    catena::DeviceComponent_ComponentLanguagePack returnVal;
     catena::exception_with_status rc("", catena::StatusCode::OK);
-    returnVal.set_language("en");
-    catena::LanguagePack *pack = returnVal.mutable_language_pack();
-    pack->set_name("English");
-    pack->mutable_words()->insert({"Hello", "Goodbye"});
 
     // Defining mock fuctions
+    expectedProceedCalls();
     EXPECT_CALL(dm, mutex()).Times(1).WillOnce(::testing::ReturnRef(mockMutex));
-    EXPECT_CALL(context, fields("language")).Times(1).WillOnce(::testing::ReturnRef(language));
-    EXPECT_CALL(dm, getLanguagePack(::testing::_, ::testing::_)).Times(1);
-    ON_CALL(dm, getLanguagePack(::testing::_, ::testing::_))
-        .WillByDefault(::testing::Invoke([&rc, &returnVal](const std::string &languageId, catena::DeviceComponent_ComponentLanguagePack &pack) {
-            pack.CopyFrom(returnVal);
+    EXPECT_CALL(dm, addLanguage(::testing::_, ::testing::_)).Times(1);
+    ON_CALL(dm, addLanguage(::testing::_, ::testing::_))
+        .WillByDefault(::testing::Invoke([&rc](catena::AddLanguagePayload &language, catena::common::Authorizer &authz) {
             return catena::exception_with_status(rc.what(), rc.status);
         }));
 
     // Calling proceed() and checking written response.
-    languagePackRequest->proceed();
-
-    std::string jsonBody;
-    google::protobuf::util::JsonPrintOptions options; // Default options
-    auto status = google::protobuf::util::MessageToJsonString(returnVal, &jsonBody, options);
-    EXPECT_EQ(readResponse(), expectedResponse(rc, jsonBody));
-}
-
-/* 
- * TEST 3 - dm.getLanguagePack() returns an error.
- */
-TEST_F(RESTLanguagePackRequestTests, ListLanguages_proceedErrReturn) {
-    // Setting up the returnVal to test with.
-    catena::exception_with_status rc("Language pack not found", catena::StatusCode::NOT_FOUND);
-
-    // Defining mock fuctions
-    EXPECT_CALL(dm, mutex()).Times(1).WillOnce(::testing::ReturnRef(mockMutex));
-    EXPECT_CALL(context, fields("language")).Times(1).WillOnce(::testing::ReturnRef(language));
-    EXPECT_CALL(dm, getLanguagePack(::testing::_, ::testing::_)).Times(1);
-    ON_CALL(dm, getLanguagePack(::testing::_, ::testing::_))
-        .WillByDefault(::testing::Invoke([&rc](const std::string &languageId, catena::DeviceComponent_ComponentLanguagePack &pack) {
-            return catena::exception_with_status(rc.what(), rc.status);
-        }));
-
-    // Calling proceed() and checking written response.
-    languagePackRequest->proceed();
+    addLanguage->proceed();
 
     EXPECT_EQ(readResponse(), expectedResponse(rc));
 }
 
 /* 
- * TEST 4 - dm.getLanguagePack() throws an error.
+ * TEST 3 - dm.addLangauge() returns an error.
  */
-TEST_F(RESTLanguagePackRequestTests, ListLanguages_proceedErrThrow) {
+TEST_F(RESTAddLanguageTests, AddLanguage_proceedErrReturn) {
     // Setting up the returnVal to test with.
-    catena::exception_with_status rc("Unknown error", catena::StatusCode::UNKNOWN);
-
+    catena::exception_with_status rc("Invalid language pack", catena::StatusCode::INVALID_ARGUMENT);
+    
     // Defining mock fuctions
+    expectedProceedCalls();
     EXPECT_CALL(dm, mutex()).Times(1).WillOnce(::testing::ReturnRef(mockMutex));
-    EXPECT_CALL(context, fields("language")).Times(1).WillOnce(::testing::ReturnRef(language));
-    EXPECT_CALL(dm, getLanguagePack(::testing::_, ::testing::_)).Times(1);
-    ON_CALL(dm, getLanguagePack(::testing::_, ::testing::_))
-        .WillByDefault(::testing::Invoke([&rc](const std::string &languageId, catena::DeviceComponent_ComponentLanguagePack &pack) {
+    EXPECT_CALL(dm, addLanguage(::testing::_, ::testing::_)).Times(1);
+    ON_CALL(dm, addLanguage(::testing::_, ::testing::_))
+        .WillByDefault(::testing::Invoke([&rc](catena::AddLanguagePayload &language, catena::common::Authorizer &authz) {
+            return catena::exception_with_status(rc.what(), rc.status);
+        }));
+
+    // Calling proceed() and checking written response.
+    addLanguage->proceed();
+
+    EXPECT_EQ(readResponse(), expectedResponse(rc));
+}
+
+/* 
+ * TEST 4 - dm.addLangauge() throws a catena error.
+ */
+TEST_F(RESTAddLanguageTests, AddLanguage_proceedErrThrowCatena) {
+    // Setting up the returnVal to test with.
+    catena::exception_with_status rc("Invalid language pack", catena::StatusCode::INVALID_ARGUMENT);
+    
+    // Defining mock fuctions
+    expectedProceedCalls();
+    EXPECT_CALL(dm, mutex()).Times(1).WillOnce(::testing::ReturnRef(mockMutex));
+    EXPECT_CALL(dm, addLanguage(::testing::_, ::testing::_)).Times(1);
+    ON_CALL(dm, addLanguage(::testing::_, ::testing::_))
+        .WillByDefault(::testing::Invoke([&rc](catena::AddLanguagePayload &language, catena::common::Authorizer &authz) {
             throw catena::exception_with_status(rc.what(), rc.status);
             return catena::exception_with_status("", catena::StatusCode::OK); // Here to ignore errors.
         }));
 
     // Calling proceed() and checking written response.
-    languagePackRequest->proceed();
+    addLanguage->proceed();
 
     EXPECT_EQ(readResponse(), expectedResponse(rc));
 }
 
 /* 
- * TEST 5 - Writing to console with LanguagePackRequest finish().
+ * TEST 5 - dm.addLangauge() throws an std error.
  */
-TEST_F(RESTLanguagePackRequestTests, LanguagePackRequest_finish) {
+TEST_F(RESTAddLanguageTests, AddLanguage_proceedErrThrowUnknown) {
+    // Setting up the returnVal to test with.
+    catena::exception_with_status rc("Unknown error", catena::StatusCode::UNKNOWN);
+
+    // Defining mock fuctions
+    expectedProceedCalls();
+    EXPECT_CALL(dm, mutex()).Times(1).WillOnce(::testing::ReturnRef(mockMutex));
+    EXPECT_CALL(dm, addLanguage(::testing::_, ::testing::_)).Times(1);
+    ON_CALL(dm, addLanguage(::testing::_, ::testing::_))
+        .WillByDefault(::testing::Invoke([&rc](catena::AddLanguagePayload &language, catena::common::Authorizer &authz) {
+            throw std::runtime_error(rc.what());
+            return catena::exception_with_status("", catena::StatusCode::OK); // Here to ignore errors.
+        }));
+
+    // Calling proceed() and checking written response.
+    addLanguage->proceed();
+
+    EXPECT_EQ(readResponse(), expectedResponse(rc));
+}
+
+/* 
+ * TEST 6 - Normal case for AddLanguage proceed() with authz.
+ */
+TEST_F(RESTAddLanguageTests, AddLanguage_proceedAuthzValid) {
+    // Setting up the returnVal to test with.
+    catena::exception_with_status rc("", catena::StatusCode::OK);
+    authz = true;
+
+    // Defining mock fuctions
+    expectedProceedCalls();
+    EXPECT_CALL(dm, mutex()).Times(1).WillOnce(::testing::ReturnRef(mockMutex));
+    EXPECT_CALL(dm, addLanguage(::testing::_, ::testing::_)).Times(1);
+    ON_CALL(dm, addLanguage(::testing::_, ::testing::_))
+        .WillByDefault(::testing::Invoke([&rc](catena::AddLanguagePayload &language, catena::common::Authorizer &authz) {
+            return catena::exception_with_status(rc.what(), rc.status);
+        }));
+
+    // Calling proceed() and checking written response.
+    addLanguage->proceed();
+
+    EXPECT_EQ(readResponse(), expectedResponse(rc));
+}
+
+/* 
+ * TEST 7 - Normal case for AddLanguage proceed() with authz.
+ */
+TEST_F(RESTAddLanguageTests, AddLanguage_proceedAuthzInvalid) {
+    // Setting up the returnVal to test with.
+    catena::exception_with_status rc("", catena::StatusCode::UNAUTHENTICATED);
+    authz = true;
+    // Not a token so it should get rejected by the authorizer.
+    mockToken = "THIS SHOULD NOT PARSE";
+
+    // Defining mock fuctions
+    expectedProceedCalls();
+
+    // Calling proceed() and checking written response.
+    addLanguage->proceed();
+
+    EXPECT_EQ(readResponse(), expectedResponse(rc));
+}
+
+/* 
+ * TEST 8 - dm.addLangauge() returns an error with authz.
+ */
+TEST_F(RESTAddLanguageTests, AddLanguage_proceedAuthzErrReturn) {
+    // Setting up the returnVal to test with.
+    catena::exception_with_status rc("Invalid language pack", catena::StatusCode::INVALID_ARGUMENT);
+    authz = true;
+
+    // Defining mock fuctions
+    expectedProceedCalls();
+    EXPECT_CALL(dm, mutex()).Times(1).WillOnce(::testing::ReturnRef(mockMutex));
+    EXPECT_CALL(dm, addLanguage(::testing::_, ::testing::_)).Times(1);
+    ON_CALL(dm, addLanguage(::testing::_, ::testing::_))
+        .WillByDefault(::testing::Invoke([&rc](catena::AddLanguagePayload &language, catena::common::Authorizer &authz) {
+            return catena::exception_with_status(rc.what(), rc.status);
+        }));
+
+    // Calling proceed() and checking written response.
+    addLanguage->proceed();
+
+    EXPECT_EQ(readResponse(), expectedResponse(rc));
+}
+
+/* 
+ * TEST 9 - dm.addLangauge() throws a catena error with authz.
+ */
+TEST_F(RESTAddLanguageTests, AddLanguage_proceedAuthzErrThrowCatena) {
+    // Setting up the returnVal to test with.
+    catena::exception_with_status rc("Invalid language pack", catena::StatusCode::INVALID_ARGUMENT);
+    authz = true;
+
+    // Defining mock fuctions
+    expectedProceedCalls();
+    EXPECT_CALL(dm, mutex()).Times(1).WillOnce(::testing::ReturnRef(mockMutex));
+    EXPECT_CALL(dm, addLanguage(::testing::_, ::testing::_)).Times(1);
+    ON_CALL(dm, addLanguage(::testing::_, ::testing::_))
+        .WillByDefault(::testing::Invoke([&rc](catena::AddLanguagePayload &language, catena::common::Authorizer &authz) {
+            throw catena::exception_with_status(rc.what(), rc.status);
+            return catena::exception_with_status("", catena::StatusCode::OK); // Here to ignore errors.
+        }));
+
+    // Calling proceed() and checking written response.
+    addLanguage->proceed();
+
+    EXPECT_EQ(readResponse(), expectedResponse(rc));
+}
+
+/* 
+ * TEST 10 - dm.addLangauge() throws an std error with authz.
+ */
+TEST_F(RESTAddLanguageTests, AddLanguage_proceedAuthzErrThrowUnkown) {
+    // Setting up the returnVal to test with.
+    catena::exception_with_status rc("Unknown error", catena::StatusCode::UNKNOWN);
+    authz = true;
+
+    // Defining mock fuctions
+    expectedProceedCalls();
+    EXPECT_CALL(dm, mutex()).Times(1).WillOnce(::testing::ReturnRef(mockMutex));
+    EXPECT_CALL(dm, addLanguage(::testing::_, ::testing::_)).Times(1);
+    ON_CALL(dm, addLanguage(::testing::_, ::testing::_))
+        .WillByDefault(::testing::Invoke([&rc](catena::AddLanguagePayload &language, catena::common::Authorizer &authz) {
+            throw std::runtime_error(rc.what());
+            return catena::exception_with_status("", catena::StatusCode::OK); // Here to ignore errors.
+        }));
+
+    // Calling proceed() and checking written response.
+    addLanguage->proceed();
+
+    EXPECT_EQ(readResponse(), expectedResponse(rc));
+}
+
+/* 
+ * TEST 11 - AddLanguage->proceed() fails to parse json body.
+ */
+TEST_F(RESTAddLanguageTests, AddLanguage_proceedFailParse) {
+    // Setting up the returnVal to test with.
+    catena::exception_with_status rc("Failed to convert JSON to protobuf", catena::StatusCode::INVALID_ARGUMENT);
+    jsonBody = "THIS IS NOT JSON";
+
+    // Defining mock functions.
+    EXPECT_CALL(context, slot()).Times(1).WillOnce(::testing::Return(1));
+    EXPECT_CALL(context, fields("id")).Times(1).WillOnce(::testing::ReturnRef(language));
+    EXPECT_CALL(context, jsonBody()).Times(1).WillOnce(::testing::ReturnRef(jsonBody));
+
+    // Calling proceed() and checking written response.
+    addLanguage->proceed();
+
+    EXPECT_EQ(readResponse(), expectedResponse(rc));
+}
+
+/* 
+ * TEST 12 - Writing to console with AddLanguage finish().
+ */
+TEST_F(RESTAddLanguageTests, AddLanguage_finish) {
     // Calling finish and expecting the console output.
-    languagePackRequest->finish();
+    addLanguage->finish();
     // Idk why I cant use .contains() here :/
-    ASSERT_TRUE(MockConsole.str().find("LanguagePackRequest[4] finished\n") != std::string::npos);
+    ASSERT_TRUE(MockConsole.str().find("AddLanguage[11] finished\n") != std::string::npos);
 }
