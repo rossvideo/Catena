004a61729803e8fd56e68563d56cef6eaa02e698
BenWhitten-RV <BenWhitten-RV@users.noreply.github.com>
2025-05-21 19:03:19 +0000

    deploy: 8704c26d45acf3ac732383b44ade55d6a5be6edc

--- doxygen/classcatena_1_1REST_1_1ISocketWriter.js
+++ doxygen/classcatena_1_1REST_1_1ISocketWriter.js
@@ -1,5 +1,5 @@
 var classcatena_1_1REST_1_1ISocketWriter =
 [
     [ "ISocketWriter", "classcatena_1_1REST_1_1ISocketWriter.html#a69830f9da3cd89430fec17d1e66576eb", null ],
-    [ "sendResponse", "classcatena_1_1REST_1_1ISocketWriter.html#a104cf73d8444c998b8d200ad3344a650", null ]
+    [ "sendResponse", "classcatena_1_1REST_1_1ISocketWriter.html#a063ef9d3b8e4fd04d3f9cbf8d2d3b9ff", null ]
 ];
