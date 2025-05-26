34d60451e859f1891418f880a76426f24ede7cd0
BenWhitten-RV <Benjamin.whitten@rossvideo.com>
2025-05-26 16:16:58 +0000

    It actually works now

--- sdks/cpp/connections/gRPC/tests/GetValue_test.cpp
+++ sdks/cpp/connections/gRPC/tests/GetValue_test.cpp
@@ -49,7 +49,7 @@
 // Test helpers
 #include "../../common/tests/CommonMockClasses.h"
 #include "gRPCMockClasses.h"
-#include "ServerHelper.h"
+// #include "ServerHelper.h"
 
 // gRPC
 #include "controllers/GetValue.h"
@@ -57,37 +57,78 @@
 using namespace catena::common;
 using namespace catena::gRPC;
 
+// Server stuff
+class MockServer {
+  public:
+    MockServer() {
+        // Creating the mock server.
+        builder.AddListeningPort(serverAddr, grpc::InsecureServerCredentials());
+        cq = builder.AddCompletionQueue();
+        builder.RegisterService(&service);
+        server = builder.BuildAndStart();
+        std::cout<<"Server created"<<std::endl;
+
+        // Creating a client.
+        channel = grpc::CreateChannel(serverAddr, grpc::InsecureChannelCredentials());
+        client = catena::CatenaService::NewStub(channel);
+
+        // Deploying cq handler on a thread.
+        cqthread = std::make_unique<std::thread>([&]() {
+            void* ignored_tag;
+            bool ignored_ok;
+            while (cq->Next(&ignored_tag, &ignored_ok)) {
+                std::cout << "Processing cq event" << std::endl;
+                if (!testCall) {
+                    testCall = asyncCall;
+                    asyncCall = nullptr;
+                }
+                testCall->proceed(ok);
+            }
+        });
+    }
+
+    ~MockServer() {
+        ok = false;
+        // Cleaning up the server.
+        server->Shutdown();
+        // Cleaning the cq
+        cq->Shutdown();
+        cqthread->join();
+        // Make sure the calldata objects were destroyed.
+        EXPECT_TRUE(!testCall);
+        EXPECT_TRUE(!asyncCall);
+    }
+
+    std::string serverAddr = "0.0.0.0:50051";
+    grpc::ServerBuilder builder;
+    std::unique_ptr<grpc::Server> server = nullptr;
+    MockServiceImpl service;
+    std::mutex mtx;
+    MockDevice dm;
+    std::unique_ptr<grpc::ServerCompletionQueue> cq = nullptr;
+    std::unique_ptr<std::thread> cqthread = nullptr;
+    ICallData* testCall = nullptr;
+    ICallData* asyncCall = nullptr;
+    std::shared_ptr<grpc::Channel> channel = nullptr;
+    std::unique_ptr<catena::CatenaService::Stub> client = nullptr;
+    bool ok = true;
+};
+
+MockServer* globalServer = nullptr;
+
 // Fixture
-class gRPCGetValueTests : public ::testing::Test, public ServerHelper {
+class gRPCGetValueTests : public ::testing::Test {
   protected:
 
     void SetUp() override {
-        // Redirecting cout to a stringstream for testing.
-        // oldCout = std::cout.rdbuf(MockConsole.rdbuf());
-
-        // Creating the server.
-        createServer();
-        createClient();
-
-        EXPECT_CALL(service, registerItem(::testing::_)).Times(1).WillOnce(::testing::Return());
-        EXPECT_CALL(service, cq()).Times(2).WillRepeatedly(::testing::Return(cq.get()));
-
-        // Creating getValue object.
-        testCall = new GetValue(&service, dm, true);
+        
     }
 
     void TearDown() override {
-        // Restoring cout
-        // std::cout.rdbuf(oldCout);
-
-        // Cleaning up.
-        shutdown();
+        
     }
 
-    std::stringstream MockConsole;
-    std::streambuf* oldCout;
 
-    std::string createMsg = "status: 0, ok: true";
 };
 
 /*
@@ -99,52 +140,76 @@ class gRPCGetValueTests : public ::testing::Test, public ServerHelper {
  */
 TEST_F(gRPCGetValueTests, GetValue_create) {
     // Making sure getValue is created from the SetUp step.
-    ASSERT_TRUE(testCall);
-    // ASSERT_TRUE(MockConsole.str().find(createMsg) != std::string::npos);
+    globalServer = new MockServer();
+    // Creating getValue object.
+    EXPECT_CALL(globalServer->service, registerItem(::testing::_)).Times(1).WillOnce(::testing::Return());
+    EXPECT_CALL(globalServer->service, cq()).Times(2).WillRepeatedly(::testing::Return(globalServer->cq.get()));
+
+    // Creating getValue object.
+    globalServer->testCall = new GetValue(&globalServer->service, globalServer->dm, true);
+
 }
 
 TEST_F(gRPCGetValueTests, GetValue_proceed) {
+    // Setting up RPC input value.
     catena::GetValuePayload inVal;
+    inVal.set_oid("/test_oid");
+    // Setting up one var as expected RPC output and one to recieve RPC output.
+    catena::Value expVal;
+    expVal.set_string_value("Test string");
     catena::Value outVal;
-    catena::Value returnVal;
-    returnVal.set_string_value("Test string");
+    // Setting up an rcs.
     catena::exception_with_status rc("", catena::StatusCode::OK);
-    grpc::ClientContext context;
+    grpc::Status expRc(static_cast<grpc::StatusCode>(rc.status), rc.what());
+    grpc::Status outRc;
+    // Client stuff.
+    grpc::ClientContext clientContext;
+    bool done = false;
+    std::condition_variable cv;
+    std::mutex cv_mtx;
+    std::unique_lock<std::mutex> lock(cv_mtx);
 
+    // Mocking functions.
     // new GetValue(service, dm, true);
-    EXPECT_CALL(service, registerItem(::testing::_)).Times(1)
+    EXPECT_CALL(globalServer->service, registerItem(::testing::_)).Times(1)
         .WillOnce(::testing::Invoke([this](ICallData* cd) {
-            asyncCall = cd; // Keep track of obj to avoid mem leak.
+            globalServer->asyncCall = cd;
         }));
-    EXPECT_CALL(service, cq()).Times(2).WillRepeatedly(::testing::Return(cq.get()));
+    EXPECT_CALL(globalServer->service, cq()).Times(2).WillRepeatedly(::testing::Return(globalServer->cq.get()));
     // kProcess();
-    EXPECT_CALL(service, authorizationEnabled()).Times(1).WillOnce(::testing::Return(false));
-    EXPECT_CALL(dm, mutex()).Times(1).WillOnce(::testing::ReturnRef(mtx));
-    EXPECT_CALL(dm, getValue(::testing::_, ::testing::_, ::testing::_)).Times(1)
-        .WillOnce(::testing::Invoke([returnVal, &rc](const std::string& jptr, catena::Value& value, Authorizer& authz) {
+    EXPECT_CALL(globalServer->service, authorizationEnabled()).Times(1).WillOnce(::testing::Return(false));
+    EXPECT_CALL(globalServer->dm, mutex()).Times(1).WillOnce(::testing::ReturnRef(globalServer->mtx));
+    EXPECT_CALL(globalServer->dm, getValue("/test_oid", ::testing::_, ::testing::_)).Times(1)
+        .WillOnce(::testing::Invoke([expVal, &rc](const std::string& jptr, catena::Value& value, Authorizer& authz) {
             std::cout<<"dm.getValue() called"<<std::endl;
-            value.CopyFrom(returnVal);
+            value.CopyFrom(expVal);
             return catena::exception_with_status(rc.what(), rc.status);
         }));
-    EXPECT_CALL(service, deregisterItem(::testing::_)).Times(1).WillOnce(::testing::Return());
+    EXPECT_CALL(globalServer->service, deregisterItem(::testing::_)).Times(1).WillOnce(::testing::Invoke([]() {
+        delete globalServer->testCall;
+        globalServer->testCall = nullptr;
+        std::cout<<"Deregistered testCall"<<std::endl;
+    }));
 
-    std::thread cqThread([this](){
-        // Waiting for request from completion queue before calling proceed.
-        void* ignored_tag;
-        bool ignored_ok;
-        cq->Next(&ignored_tag, &ignored_ok);
-        testCall->proceed(true);
-        cq->Next(&ignored_tag, &ignored_ok);
-        testCall->proceed(true);
-    });
-
-    stub->async()->GetValue(&context, &inVal, &outVal, [](grpc::Status status){ 
+    // Sending the RPC.
+    globalServer->client->async()->GetValue(&clientContext, &inVal, &outVal, [&cv, &done, &outRc](grpc::Status status){ 
         std::cout<<"In the lambda function"<<std::endl;
+        outRc = status;
+        done = true;
+        cv.notify_one();
     });
+    std::cout<<"Waiting for the lambda to finish"<<std::endl;
+    cv.wait(lock, [&done] { return done; });
 
-    cqThread.join();
-    std::cout<<"AsyncGetValue() done." << std::endl;
+    // Comparing output.
+    EXPECT_EQ(outRc.error_code(), expRc.error_code());
 }
 
-
-
+TEST_F(gRPCGetValueTests, GetValue_shutdown) {
+    EXPECT_CALL(globalServer->service, deregisterItem(::testing::_)).Times(1).WillOnce(::testing::Invoke([]() {
+        delete globalServer->testCall;
+        globalServer->testCall = nullptr;
+        std::cout<<"Deregistered testCall"<<std::endl;
+    }));
+    delete globalServer;
+}
