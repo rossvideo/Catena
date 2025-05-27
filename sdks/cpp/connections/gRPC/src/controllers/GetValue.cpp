ad228853ca5a0f72fa4ff59d47c18d0925fece83
BenWhitten-RV <Benjamin.whitten@rossvideo.com>
2025-05-27 14:16:35 -0400

    Update sdks/cpp/connections/gRPC/src/controllers/GetValue.cpp
    Co-authored-by: Nathan R <nathan.rochon@rossvideo.com>
    Signed-off-by: BenWhitten-RV <Benjamin.whitten@rossvideo.com>

--- sdks/cpp/connections/gRPC/src/controllers/GetValue.cpp
+++ sdks/cpp/connections/gRPC/src/controllers/GetValue.cpp
@@ -118,7 +118,11 @@ void GetValue::proceed( bool ok) {
          * This should be impossible to reach.
          */
         default:
+        default:// GCOVR_EXCL_START
             status_ = CallStatus::kFinish;
+            grpc::Status errorStatus(grpc::StatusCode::INTERNAL, "illegal state");
+            responder_.FinishWithError(errorStatus, this);
+            // GCOVR_EXCL_STOP
             grpc::Status errorStatus(grpc::StatusCode::INTERNAL, "illegal state");
             responder_.FinishWithError(errorStatus, this);
     }
