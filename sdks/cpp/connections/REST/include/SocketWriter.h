e653dc402e66c2ce2054a5494c212e6ddc04b3af
BenWhitten-RV <Benjamin.whitten@rossvideo.com>
2025-05-30 18:40:20 +0000

    SocketWriter now hypothetically supports buffered stream responses

--- sdks/cpp/connections/REST/include/SocketWriter.h
+++ sdks/cpp/connections/REST/include/SocketWriter.h
@@ -69,7 +69,7 @@ class SocketWriter : public ISocketWriter {
      * @param socket The socket to write to.
      * @param origin The origin of the request.
      */
-    SocketWriter(tcp::socket& socket, const std::string& origin = "*") : socket_{socket}, origin_{origin} {}
+    SocketWriter(tcp::socket& socket, const std::string& origin = "*", bool buffer = false) : socket_{socket}, origin_{origin}, buffer_{buffer} {}
 
     /**
      * @brief Finishes writing the HTTP response.
@@ -87,6 +87,15 @@ class SocketWriter : public ISocketWriter {
      * @brief The origin of the request.
      */
     std::string origin_;
+
+    /**
+     * @brief flag to indicate whether to buffer a multi-message response.
+     */
+    bool buffer_;
+    /**
+     * @brief The json body of the response to write to the client.
+     */
+    std::string jsonBody_ = "";
 };
 
 /**
