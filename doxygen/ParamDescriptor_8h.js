f2bd4323b60bacdcd1de85e6212b9eeb06875ad5
BenWhitten-RV <BenWhitten-RV@users.noreply.github.com>
2025-06-04 17:54:30 +0000

    deploy: 92d12bd2d35593a870c9ffd33c4a63d129d5aef6

--- doxygen/ParamDescriptor_8h.js
+++ doxygen/ParamDescriptor_8h.js
@@ -1,4 +1,6 @@
 var ParamDescriptor_8h =
 [
-    [ "catena::common::ParamDescriptor", "classcatena_1_1common_1_1ParamDescriptor.html", "classcatena_1_1common_1_1ParamDescriptor" ]
+    [ "catena::common::ParamDescriptor", "classcatena_1_1common_1_1ParamDescriptor.html", "classcatena_1_1common_1_1ParamDescriptor" ],
+    [ "catena::common::ParamDescriptor::CommandResponder", "classcatena_1_1common_1_1ParamDescriptor_1_1CommandResponder.html", "classcatena_1_1common_1_1ParamDescriptor_1_1CommandResponder" ],
+    [ "catena::common::ParamDescriptor::CommandResponder::promise_type", "structcatena_1_1common_1_1ParamDescriptor_1_1CommandResponder_1_1promise__type.html", "structcatena_1_1common_1_1ParamDescriptor_1_1CommandResponder_1_1promise__type" ]
 ];
