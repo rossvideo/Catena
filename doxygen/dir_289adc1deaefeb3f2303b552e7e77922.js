ede8dc4a30498e11d84057e612c131177568aaf9
ctwarog-ross <ctwarog-ross@users.noreply.github.com>
2025-06-06 18:05:38 +0000

    deploy: 8da70e16111b33d4c83b51bd477756d719b91841

---
+++ doxygen/dir_289adc1deaefeb3f2303b552e7e77922.js
@@ -0,0 +1,8 @@
+var dir_289adc1deaefeb3f2303b552e7e77922 =
+[
+    [ "controllers", "dir_b53d663f71ce77f01593ad051de20c34.html", "dir_b53d663f71ce77f01593ad051de20c34" ],
+    [ "interface", "dir_1df3d5d21152550c6725e433ad08eccb.html", "dir_1df3d5d21152550c6725e433ad08eccb" ],
+    [ "ServiceImpl.h", "REST_2include_2ServiceImpl_8h.html", "REST_2include_2ServiceImpl_8h" ],
+    [ "SocketReader.h", "SocketReader_8h.html", "SocketReader_8h" ],
+    [ "SocketWriter.h", "SocketWriter_8h.html", "SocketWriter_8h" ]
+];
--- doxygen/dir_b30cdd110ff3b21e3f12ebbadb6f4032.js
+++
@@ -1,5 +0,0 @@
-var dir_b30cdd110ff3b21e3f12ebbadb6f4032 =
-[
-    [ "ICallData.h", "gRPC_2include_2interface_2ICallData_8h.html", "gRPC_2include_2interface_2ICallData_8h" ],
-    [ "IServiceImpl.h", "gRPC_2include_2interface_2IServiceImpl_8h.html", "gRPC_2include_2interface_2IServiceImpl_8h" ]
-];
