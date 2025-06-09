86f75f12a5c01ead83933f76f0d83685d17c1243
BenWhitten-RV <BenWhitten-RV@users.noreply.github.com>
2025-06-09 15:11:50 +0000

    deploy: 7a01aab0f4167292518271f7dcd6e9dc34a03ccc

--- doxygen/classcatena_1_1REST_1_1SocketReader.js
+++ doxygen/classcatena_1_1REST_1_1SocketReader.js
@@ -11,9 +11,9 @@ var classcatena_1_1REST_1_1SocketReader =
     [ "hasField", "classcatena_1_1REST_1_1SocketReader.html#ae670c997ccfa8dbd2054a2af37d4404f", null ],
     [ "jsonBody", "classcatena_1_1REST_1_1SocketReader.html#a0b4655c8cf81045dd065ec6d1dca661f", null ],
     [ "jwsToken", "classcatena_1_1REST_1_1SocketReader.html#ac3c78ea250b9d57087297eead2bdb7e0", null ],
+    [ "language", "classcatena_1_1REST_1_1SocketReader.html#a59e5e35b9b9d421e186252caf9bd552b", null ],
     [ "method", "classcatena_1_1REST_1_1SocketReader.html#a83236f1c3ab45215d0b34d4b6a2fa16e", null ],
     [ "origin", "classcatena_1_1REST_1_1SocketReader.html#a6da93bb5940bcd28e17f07da9713d23b", null ],
-    [ "read", "classcatena_1_1REST_1_1SocketReader.html#a706a463ec32c41305a1c8da137a7ce70", null ],
-    [ "slot", "classcatena_1_1REST_1_1SocketReader.html#aa77a5dcf3caf0ae527fa88e73dc3fdf1", null ],
-    [ "stream", "classcatena_1_1REST_1_1SocketReader.html#ab8e5d7003da0cf636e385a81a72a41cb", null ]
+    [ "read", "classcatena_1_1REST_1_1SocketReader.html#af2bcedddfd452f6c746058477dd3258c", null ],
+    [ "slot", "classcatena_1_1REST_1_1SocketReader.html#aa77a5dcf3caf0ae527fa88e73dc3fdf1", null ]
 ];
