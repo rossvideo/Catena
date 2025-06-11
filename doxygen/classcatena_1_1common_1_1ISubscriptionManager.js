55043e4283df740853ed5042ab46864e335e74ce
BenWhitten-RV <BenWhitten-RV@users.noreply.github.com>
2025-06-11 17:24:59 +0000

    deploy: f11b15de58a6c60b7639b6caff627af4ab4c7409

--- doxygen/classcatena_1_1common_1_1ISubscriptionManager.js
+++ doxygen/classcatena_1_1common_1_1ISubscriptionManager.js
@@ -1,7 +1,8 @@
 var classcatena_1_1common_1_1ISubscriptionManager =
 [
     [ "addSubscription", "classcatena_1_1common_1_1ISubscriptionManager.html#a86952cbef9d7428bfde4ec19b278a827", null ],
-    [ "getAllSubscribedOids", "classcatena_1_1common_1_1ISubscriptionManager.html#a7f760651efc5d501195e07ef398720ed", null ],
+    [ "getAllSubscribedOids", "classcatena_1_1common_1_1ISubscriptionManager.html#a1167c1614e23045b12c5b69c3465f16c", null ],
+    [ "isSubscribed", "classcatena_1_1common_1_1ISubscriptionManager.html#a2306837ba01d990d283dbbed75b0a992", null ],
     [ "isWildcard", "classcatena_1_1common_1_1ISubscriptionManager.html#ac05b965006e98cf193568bce2f25ee40", null ],
-    [ "removeSubscription", "classcatena_1_1common_1_1ISubscriptionManager.html#afcf85504a5bfc1d35b0b5804e06e9b62", null ]
+    [ "removeSubscription", "classcatena_1_1common_1_1ISubscriptionManager.html#a5717b4ffb75dde65f71bf1900fc6ab32", null ]
 ];
