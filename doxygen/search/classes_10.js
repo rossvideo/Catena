0a86db4b3b9194f6f43473795715aa5e8e4ed1cd
BenWhitten-RV <BenWhitten-RV@users.noreply.github.com>
2025-06-13 15:43:57 +0000

    deploy: ca27b70194e06c9ec1c9a8bef370f3980d9e1c4e

--- doxygen/search/classes_10.js
+++ doxygen/search/classes_10.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['updatesubscriptions_0',['updatesubscriptions',['../classcatena_1_1gRPC_1_1UpdateSubscriptions.html',1,'catena::gRPC::UpdateSubscriptions'],['../classcatena_1_1REST_1_1UpdateSubscriptions.html',1,'catena::REST::UpdateSubscriptions']]]
+  ['updatesubscriptions_0',['UpdateSubscriptions',['../classcatena_1_1gRPC_1_1UpdateSubscriptions.html',1,'catena::gRPC']]]
 ];
