ba1b99b9a8d72ac7c244870b83ff07929703d985
ctwarog-ross <ctwarog-ross@users.noreply.github.com>
2025-06-13 15:01:06 +0000

    deploy: a70f23d17cd807611868ae4cf721bb43b3ae2015

---
+++ doxygen/classcatena_1_1REST_1_1AddLanguage.js
@@ -0,0 +1,6 @@
+var classcatena_1_1REST_1_1AddLanguage =
+[
+    [ "AddLanguage", "classcatena_1_1REST_1_1AddLanguage.html#a09ab8e7887b01614429dfa66d3ba1c16", null ],
+    [ "finish", "classcatena_1_1REST_1_1AddLanguage.html#a73f3030eb858a9847c38271c3a9a4374", null ],
+    [ "proceed", "classcatena_1_1REST_1_1AddLanguage.html#a484ce62318daf82db48a5956f485174d", null ]
+];
