5e612f081f69e198ca30adb15d692b2b5c1218ec
ZuhayrSarkerRV <ZuhayrSarkerRV@users.noreply.github.com>
2025-06-16 16:19:30 +0000

    deploy: 5d0a6e2ef82c89ee84152b430c7e2549338474c3

--- doxygen/classcatena_1_1common_1_1ISubscriptionManager.js
+++ doxygen/classcatena_1_1common_1_1ISubscriptionManager.js
@@ -1,8 +1,7 @@
 var classcatena_1_1common_1_1ISubscriptionManager =
 [
-    [ "addSubscription", "classcatena_1_1common_1_1ISubscriptionManager.html#a86952cbef9d7428bfde4ec19b278a827", null ],
-    [ "getAllSubscribedOids", "classcatena_1_1common_1_1ISubscriptionManager.html#a1167c1614e23045b12c5b69c3465f16c", null ],
-    [ "isSubscribed", "classcatena_1_1common_1_1ISubscriptionManager.html#a2306837ba01d990d283dbbed75b0a992", null ],
+    [ "addSubscription", "classcatena_1_1common_1_1ISubscriptionManager.html#aa19f9478c082691639e58623e7df433d", null ],
+    [ "getAllSubscribedOids", "classcatena_1_1common_1_1ISubscriptionManager.html#ab5d55752a99c745909efca91f2931987", null ],
     [ "isWildcard", "classcatena_1_1common_1_1ISubscriptionManager.html#ac05b965006e98cf193568bce2f25ee40", null ],
-    [ "removeSubscription", "classcatena_1_1common_1_1ISubscriptionManager.html#a5717b4ffb75dde65f71bf1900fc6ab32", null ]
+    [ "removeSubscription", "classcatena_1_1common_1_1ISubscriptionManager.html#afcf85504a5bfc1d35b0b5804e06e9b62", null ]
 ];
--- doxygen/gRPC_2include_2controllers_2UpdateSubscriptions_8h.js
+++ doxygen/gRPC_2include_2controllers_2UpdateSubscriptions_8h.js
@@ -1,4 +1,4 @@
-var UpdateSubscriptions_8h =
+var gRPC_2include_2controllers_2UpdateSubscriptions_8h =
 [
     [ "catena::gRPC::UpdateSubscriptions", "classcatena_1_1gRPC_1_1UpdateSubscriptions.html", "classcatena_1_1gRPC_1_1UpdateSubscriptions" ]
 ];
