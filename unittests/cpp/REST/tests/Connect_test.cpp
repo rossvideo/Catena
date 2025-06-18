4101a4528bf028c1455c64daaca58e3977623b3a
Zuhayr Sarker <zuhayr.sarker@rossvideo.com>
2025-06-18 12:33:03 -0400

    tweaks

--- unittests/cpp/REST/tests/Connect_test.cpp
+++ unittests/cpp/REST/tests/Connect_test.cpp
@@ -12,42 +12,37 @@ using namespace catena::REST;
 using namespace catena::common;
 using namespace testing;
 
-class ConnectTest : public ::testing::Test, public RESTTest {
+class RESTConnectTest : public ::testing::Test, public RESTTest {
 protected:
-    ConnectTest() : RESTTest(&serverSocket, &clientSocket) {}
+    RESTConnectTest() : RESTTest(&serverSocket, &clientSocket) {}
 
     void SetUp() override {
-        // Create mock objects
-        socket_reader_ = std::make_unique<MockSocketReader>();
-        device_ = std::make_unique<MockDevice>();
-        subscription_manager_ = std::make_unique<MockSubscriptionManager>();
-        
         // Set up common expectations
         setupCommonExpectations();
         
         // Create Connect instance
-        connect_ = std::make_unique<catena::REST::Connect>(serverSocket, *socket_reader_, *device_);
+        connect_ = catena::REST::Connect::makeOne(serverSocket, socket_reader_, device_);
     }
 
     void TearDown() override {
-        connect_.reset();
-        socket_reader_.reset();
-        device_.reset();
+        if (connect_) {
+            delete connect_;
+        }
     }
 
     // Helper function to set up common mock expectations
     void setupCommonExpectations() {
-        EXPECT_CALL(*socket_reader_, detailLevel())
+        EXPECT_CALL(socket_reader_, detailLevel())
             .WillRepeatedly(Return(catena::Device_DetailLevel::Device_DetailLevel_FULL));
-        EXPECT_CALL(*device_, slot())
+        EXPECT_CALL(device_, slot())
             .WillRepeatedly(Return(1));
-        EXPECT_CALL(*socket_reader_, getSubscriptionManager())
-            .WillRepeatedly(ReturnRef(*subscription_manager_));
-        EXPECT_CALL(*socket_reader_, origin())
+        EXPECT_CALL(socket_reader_, getSubscriptionManager())
+            .WillRepeatedly(ReturnRef(subscription_manager_));
+        EXPECT_CALL(socket_reader_, origin())
             .WillRepeatedly(ReturnRef(origin));
-        EXPECT_CALL(*socket_reader_, fields("user_agent"))
+        EXPECT_CALL(socket_reader_, fields("user_agent"))
             .WillRepeatedly(ReturnRef(user_agent_));
-        EXPECT_CALL(*socket_reader_, hasField("force_connection"))
+        EXPECT_CALL(socket_reader_, hasField("force_connection"))
             .WillRepeatedly(Return(false));
     }
 
@@ -91,10 +86,10 @@ protected:
         auto status = google::protobuf::util::MessageToJsonString(updateResponse, &updateJson, options);
         return updateJson;
     }
-    std::unique_ptr<MockSocketReader> socket_reader_;
-    std::unique_ptr<MockDevice> device_;
-    std::unique_ptr<catena::REST::Connect> connect_;
-    std::unique_ptr<MockSubscriptionManager> subscription_manager_;
+    MockSocketReader socket_reader_;
+    MockDevice device_;
+    catena::REST::ICallData* connect_ = nullptr;
+    MockSubscriptionManager subscription_manager_;
     std::string user_agent_ = "test_agent";
     std::string paramOid_ = "test_param";
 };
@@ -102,18 +97,18 @@ protected:
 // --- 0. INITIAL TESTS ---
 
 // Test 0.1: Test constructor initialization
-TEST_F(ConnectTest, ConstructorInitialization) {
+TEST_F(RESTConnectTest, ConstructorInitialization) {
     ASSERT_NE(connect_, nullptr);
 }
 
 // Test 0.2: Test unauthorized connection
-TEST_F(ConnectTest, ProceedHandlesAuthzError) {
+TEST_F(RESTConnectTest, ProceedHandlesAuthzError) {
     std::string mockToken = "invalid_token";
     catena::exception_with_status rc("", catena::StatusCode::UNAUTHENTICATED);
 
-    EXPECT_CALL(*socket_reader_, authorizationEnabled())
+    EXPECT_CALL(socket_reader_, authorizationEnabled())
         .WillOnce(Return(true));
-    EXPECT_CALL(*socket_reader_, jwsToken())
+    EXPECT_CALL(socket_reader_, jwsToken())
         .WillOnce(ReturnRef(mockToken));
     
     connect_->proceed();
@@ -121,7 +116,7 @@ TEST_F(ConnectTest, ProceedHandlesAuthzError) {
 }
 
 // Test 0.3: Test authorized connection
-TEST_F(ConnectTest, ProceedHandlesValidAuthz) {
+TEST_F(RESTConnectTest, ProceedHandlesValidAuthz) {
     std::string mockToken = "eyJhbGciOiJSUzI1NiIsInR5cCI6ImF0K2p3dCJ9.eyJzdWIi"
                             "OiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwic2Nvc"
                             "GUiOiJzdDIxMzg6bW9uOncgc3QyMTM4Om9wOncgc3QyMTM4Om"
@@ -136,13 +131,13 @@ TEST_F(ConnectTest, ProceedHandlesValidAuthz) {
                             "dvJH-cx1s146M27UmngWUCWH6dWHaT2au9en2zSFrcWHw";
     catena::exception_with_status rc("", catena::StatusCode::OK);
 
-    EXPECT_CALL(*socket_reader_, authorizationEnabled())
+    EXPECT_CALL(socket_reader_, authorizationEnabled())
         .WillOnce(Return(true));
-    EXPECT_CALL(*socket_reader_, jwsToken())
+    EXPECT_CALL(socket_reader_, jwsToken())
         .WillOnce(ReturnRef(mockToken));
-    EXPECT_CALL(*device_, slot())
+    EXPECT_CALL(device_, slot())
         .WillRepeatedly(Return(1));
-    EXPECT_CALL(*socket_reader_, detailLevel())
+    EXPECT_CALL(socket_reader_, detailLevel())
         .WillRepeatedly(Return(catena::Device_DetailLevel::Device_DetailLevel_FULL));
 
     std::string slotJson = buildSlotResponse(1);
@@ -163,19 +158,19 @@ TEST_F(ConnectTest, ProceedHandlesValidAuthz) {
 // --- 1. SIGNAL TESTS ---
 
 // Test 1.1: Test value set by server signal
-TEST_F(ConnectTest, HandlesValueSetByServer) {
+TEST_F(RESTConnectTest, HandlesValueSetByServer) {
     std::string mockToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxM"
                             "jM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwic2NvcGUiOiJ"
                             "zdDIxMzg6bW9uIiwiaWF0IjoxNTE2MjM5MDIyfQ.YkqS7hCxst"
                             "pXulFnR98q0m088pUj6Cnf5vW6xPX8aBQ";
 
-    EXPECT_CALL(*socket_reader_, authorizationEnabled())
+    EXPECT_CALL(socket_reader_, authorizationEnabled())
         .WillOnce(Return(true));
-    EXPECT_CALL(*socket_reader_, jwsToken())
+    EXPECT_CALL(socket_reader_, jwsToken())
         .WillOnce(ReturnRef(mockToken));
-    EXPECT_CALL(*device_, slot())
+    EXPECT_CALL(device_, slot())
         .WillRepeatedly(Return(1));
-    EXPECT_CALL(*socket_reader_, detailLevel())
+    EXPECT_CALL(socket_reader_, detailLevel())
         .WillRepeatedly(Return(catena::Device_DetailLevel::Device_DetailLevel_FULL));
 
     auto param = std::make_unique<MockParam>();
@@ -190,7 +185,7 @@ TEST_F(ConnectTest, HandlesValueSetByServer) {
         }));
 
     std::set<std::string> subscribed_oids{paramOid_};
-    EXPECT_CALL(*subscription_manager_, getAllSubscribedOids(::testing::Ref(*device_)))
+    EXPECT_CALL(subscription_manager_, getAllSubscribedOids(::testing::Ref(device_)))
         .WillRepeatedly(Return(subscribed_oids));
 
     catena::exception_with_status rc("", catena::StatusCode::OK);
@@ -203,7 +198,7 @@ TEST_F(ConnectTest, HandlesValueSetByServer) {
     });
     std::this_thread::sleep_for(std::chrono::milliseconds(1));
 
-    device_->valueSetByServer.emit(paramOid_, param.get());
+    device_.valueSetByServer.emit(paramOid_, param.get());
     std::this_thread::sleep_for(std::chrono::milliseconds(1));
 
     EXPECT_EQ(readResponse(), expectedSSEResponse(rc, {slotJson, updateJson}));
@@ -214,19 +209,19 @@ TEST_F(ConnectTest, HandlesValueSetByServer) {
 }
 
 // Test 1.2: Test value set by client signal
-TEST_F(ConnectTest, HandlesValueSetByClient) {
+TEST_F(RESTConnectTest, HandlesValueSetByClient) {
     std::string mockToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxM"
                             "jM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwic2NvcGUiOiJ"
                             "zdDIxMzg6bW9uIiwiaWF0IjoxNTE2MjM5MDIyfQ.YkqS7hCxst"
                             "pXulFnR98q0m088pUj6Cnf5vW6xPX8aBQ";
 
-    EXPECT_CALL(*socket_reader_, authorizationEnabled())
+    EXPECT_CALL(socket_reader_, authorizationEnabled())
         .WillOnce(Return(true));
-    EXPECT_CALL(*socket_reader_, jwsToken())
+    EXPECT_CALL(socket_reader_, jwsToken())
         .WillOnce(ReturnRef(mockToken));
-    EXPECT_CALL(*device_, slot())
+    EXPECT_CALL(device_, slot())
         .WillRepeatedly(Return(1));
-    EXPECT_CALL(*socket_reader_, detailLevel())
+    EXPECT_CALL(socket_reader_, detailLevel())
         .WillRepeatedly(Return(catena::Device_DetailLevel::Device_DetailLevel_FULL));
 
     auto param = std::make_unique<MockParam>();
@@ -241,7 +236,7 @@ TEST_F(ConnectTest, HandlesValueSetByClient) {
         }));
 
     std::set<std::string> subscribed_oids{paramOid_};
-    EXPECT_CALL(*subscription_manager_, getAllSubscribedOids(::testing::Ref(*device_)))
+    EXPECT_CALL(subscription_manager_, getAllSubscribedOids(::testing::Ref(device_)))
         .WillRepeatedly(Return(subscribed_oids));
 
     catena::exception_with_status rc("", catena::StatusCode::OK);
@@ -254,7 +249,7 @@ TEST_F(ConnectTest, HandlesValueSetByClient) {
     });
     std::this_thread::sleep_for(std::chrono::milliseconds(1));
 
-    device_->valueSetByClient.emit(paramOid_, param.get());
+    device_.valueSetByClient.emit(paramOid_, param.get());
     std::this_thread::sleep_for(std::chrono::milliseconds(1));
 
     EXPECT_EQ(readResponse(), expectedSSEResponse(rc, {slotJson, updateJson}));
@@ -265,19 +260,19 @@ TEST_F(ConnectTest, HandlesValueSetByClient) {
 }
 
 // Test 1.3: Test language signal
-TEST_F(ConnectTest, HandlesLanguage) {
+TEST_F(RESTConnectTest, HandlesLanguage) {
     std::string mockToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxM"
                             "jM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwic2NvcGUiOiJ"
                             "zdDIxMzg6bW9uIiwiaWF0IjoxNTE2MjM5MDIyfQ.YkqS7hCxst"
                             "pXulFnR98q0m088pUj6Cnf5vW6xPX8aBQ";
 
-    EXPECT_CALL(*socket_reader_, authorizationEnabled())
+    EXPECT_CALL(socket_reader_, authorizationEnabled())
         .WillOnce(Return(true));
-    EXPECT_CALL(*socket_reader_, jwsToken())
+    EXPECT_CALL(socket_reader_, jwsToken())
         .WillOnce(ReturnRef(mockToken));
-    EXPECT_CALL(*device_, slot())
+    EXPECT_CALL(device_, slot())
         .WillRepeatedly(Return(1));
-    EXPECT_CALL(*socket_reader_, detailLevel())
+    EXPECT_CALL(socket_reader_, detailLevel())
         .WillRepeatedly(Return(catena::Device_DetailLevel::Device_DetailLevel_FULL));
 
     auto languagePack = std::make_unique<MockLanguagePack>();
@@ -297,7 +292,7 @@ TEST_F(ConnectTest, HandlesLanguage) {
     });
     std::this_thread::sleep_for(std::chrono::milliseconds(1));
 
-    device_->languageAddedPushUpdate.emit(languagePack.get());
+    device_.languageAddedPushUpdate.emit(languagePack.get());
     std::this_thread::sleep_for(std::chrono::milliseconds(1));
 
     EXPECT_EQ(readResponse(), expectedSSEResponse(rc, {slotJson, updateJson}));
@@ -310,11 +305,11 @@ TEST_F(ConnectTest, HandlesLanguage) {
 // --- 2. FINISH TESTS ---
 
 // Test 2.1: Test normal finish behavior
-TEST_F(ConnectTest, FinishClosesConnection) {
+TEST_F(RESTConnectTest, FinishClosesConnection) {
     static const std::string empty_token;
-    EXPECT_CALL(*socket_reader_, authorizationEnabled())
+    EXPECT_CALL(socket_reader_, authorizationEnabled())
         .WillOnce(Return(false));
-    EXPECT_CALL(*socket_reader_, jwsToken())
+    EXPECT_CALL(socket_reader_, jwsToken())
         .WillOnce(ReturnRef(empty_token));
     
     // Run proceed() in a separate thread since it blocks
@@ -333,11 +328,11 @@ TEST_F(ConnectTest, FinishClosesConnection) {
 }
 
 // Test 2.2: Test finish with active signal handlers
-TEST_F(ConnectTest, FinishDisconnectsSignalHandlers) {
+TEST_F(RESTConnectTest, FinishDisconnectsSignalHandlers) {
     static const std::string empty_token;
-    EXPECT_CALL(*socket_reader_, authorizationEnabled())
+    EXPECT_CALL(socket_reader_, authorizationEnabled())
         .WillOnce(Return(false));
-    EXPECT_CALL(*socket_reader_, jwsToken())
+    EXPECT_CALL(socket_reader_, jwsToken())
         .WillOnce(ReturnRef(empty_token));
     
     // Run proceed() in a separate thread since it blocks
@@ -354,8 +349,8 @@ TEST_F(ConnectTest, FinishDisconnectsSignalHandlers) {
 
     // Verify signal handlers are disconnected by attempting to emit signals
     auto param = std::make_unique<MockParam>();
-    device_->valueSetByServer.emit("test_oid", param.get());
-    device_->valueSetByClient.emit("test_oid", param.get());
+    device_.valueSetByServer.emit("test_oid", param.get());
+    device_.valueSetByClient.emit("test_oid", param.get());
     
     EXPECT_EQ(readResponse(), expectedSSEResponse(catena::exception_with_status("", catena::StatusCode::OK), {slotJson}));
 
@@ -366,10 +361,10 @@ TEST_F(ConnectTest, FinishDisconnectsSignalHandlers) {
 // --- 3. EXCEPTION TESTS ---
 
 // Test 3.1: Test catena::exception_with_status handling
-TEST_F(ConnectTest, ProceedHandlesCatenaException) {
-    EXPECT_CALL(*socket_reader_, authorizationEnabled())
+TEST_F(RESTConnectTest, ProceedHandlesCatenaException) {
+    EXPECT_CALL(socket_reader_, authorizationEnabled())
         .WillOnce(Return(true));
-    EXPECT_CALL(*socket_reader_, jwsToken())
+    EXPECT_CALL(socket_reader_, jwsToken())
         .WillOnce(Invoke([]() -> const std::string& {
             static const std::string empty;
             throw catena::exception_with_status("Auth error", catena::StatusCode::UNAUTHENTICATED);
@@ -389,10 +384,10 @@ TEST_F(ConnectTest, ProceedHandlesCatenaException) {
 }
 
 // Test 3.2: Test std::exception handling
-TEST_F(ConnectTest, ProceedHandlesStdException) {
-    EXPECT_CALL(*socket_reader_, authorizationEnabled())
+TEST_F(RESTConnectTest, ProceedHandlesStdException) {
+    EXPECT_CALL(socket_reader_, authorizationEnabled())
         .WillOnce(Return(true));
-    EXPECT_CALL(*socket_reader_, jwsToken())
+    EXPECT_CALL(socket_reader_, jwsToken())
         .WillOnce(Invoke([]() -> const std::string& {
             static const std::string empty;
             throw std::runtime_error("Runtime error");
@@ -413,10 +408,10 @@ TEST_F(ConnectTest, ProceedHandlesStdException) {
 }
 
 // Test 3.3: Test unknown exception handling
-TEST_F(ConnectTest, ProceedHandlesUnknownException) {
-    EXPECT_CALL(*socket_reader_, authorizationEnabled())
+TEST_F(RESTConnectTest, ProceedHandlesUnknownException) {
+    EXPECT_CALL(socket_reader_, authorizationEnabled())
         .WillOnce(Return(true));
-    EXPECT_CALL(*socket_reader_, jwsToken())
+    EXPECT_CALL(socket_reader_, jwsToken())
         .WillOnce(Invoke([]() -> const std::string& {
             static const std::string empty;
             throw 42; // Throw an int to trigger catch(...)
@@ -437,19 +432,19 @@ TEST_F(ConnectTest, ProceedHandlesUnknownException) {
 }
 
 // Test 3.4: Test socket close during response sending with writer failure
-TEST_F(ConnectTest, ProceedHandlesWriterFailure) {
+TEST_F(RESTConnectTest, ProceedHandlesWriterFailure) {
     std::string mockToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxM"
                             "jM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwic2NvcGUiOiJ"
                             "zdDIxMzg6bW9uIiwiaWF0IjoxNTE2MjM5MDIyfQ.YkqS7hCxst"
                             "pXulFnR98q0m088pUj6Cnf5vW6xPX8aBQ";
 
-    EXPECT_CALL(*socket_reader_, authorizationEnabled())
+    EXPECT_CALL(socket_reader_, authorizationEnabled())
         .WillOnce(Return(true));
-    EXPECT_CALL(*socket_reader_, jwsToken())
+    EXPECT_CALL(socket_reader_, jwsToken())
         .WillOnce(ReturnRef(mockToken));
-    EXPECT_CALL(*device_, slot())
+    EXPECT_CALL(device_, slot())
         .WillRepeatedly(Return(1));
-    EXPECT_CALL(*socket_reader_, detailLevel())
+    EXPECT_CALL(socket_reader_, detailLevel())
         .WillRepeatedly(Return(catena::Device_DetailLevel::Device_DetailLevel_FULL));
 
     auto param = std::make_unique<MockParam>();
@@ -464,7 +459,7 @@ TEST_F(ConnectTest, ProceedHandlesWriterFailure) {
         }));
 
     std::set<std::string> subscribed_oids{paramOid_};
-    EXPECT_CALL(*subscription_manager_, getAllSubscribedOids(::testing::Ref(*device_)))
+    EXPECT_CALL(subscription_manager_, getAllSubscribedOids(::testing::Ref(device_)))
         .WillRepeatedly(Return(subscribed_oids));
 
     // Run proceed() in a separate thread since it blocks
@@ -476,7 +471,7 @@ TEST_F(ConnectTest, ProceedHandlesWriterFailure) {
     clientSocket.close();
     std::this_thread::sleep_for(std::chrono::milliseconds(1));
 
-    device_->valueSetByServer.emit(paramOid_, param.get());
+    device_.valueSetByServer.emit(paramOid_, param.get());
     std::this_thread::sleep_for(std::chrono::milliseconds(10));
 
     EXPECT_FALSE(serverSocket.is_open());
