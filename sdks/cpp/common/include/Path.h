ccb5422ad48d4a1983d9230813fbe3b247bb95e1
BenWhitten-RV <Benjamin.whitten@rossvideo.com>
2025-07-04 19:19:52 +0000

    more fixed comments

--- sdks/cpp/common/include/Path.h
+++ sdks/cpp/common/include/Path.h
@@ -5,7 +5,7 @@
 #endif
 
 /*
- * Copyright 2024 Ross Video Ltd
+ * Copyright 2025 Ross Video Ltd
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -37,7 +37,7 @@
 /**
  * @brief Handles Path objects used to uniquely identify and access OIDs
  * @file Path.h
- * @copyright Copyright © 2023 Ross Video Ltd
+ * @copyright Copyright © 2025 Ross Video Ltd
  * @author John R. Naylor (john.naylor@rossvideo.com)
  */
 
