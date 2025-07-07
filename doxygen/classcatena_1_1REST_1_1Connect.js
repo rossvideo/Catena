3e7c5cefafdeb4d99b1098600c0b706dfb95c499
BenWhitten-RV <BenWhitten-RV@users.noreply.github.com>
2025-07-07 20:22:23 +0000

    deploy: d5553af17dbb84a1a5033bb35edd12c1757a1c54

--- doxygen/classcatena_1_1REST_1_1Connect.js
+++ doxygen/classcatena_1_1REST_1_1Connect.js
@@ -2,6 +2,5 @@ var classcatena_1_1REST_1_1Connect =
 [
     [ "Connect", "classcatena_1_1REST_1_1Connect.html#a92a806d0a843553b507c4aff86c12971", null ],
     [ "~Connect", "classcatena_1_1REST_1_1Connect.html#a2df537cb3d379910d52ba14ced454b24", null ],
-    [ "finish", "classcatena_1_1REST_1_1Connect.html#ae11e8460525080f9995fef01f01cc30f", null ],
     [ "proceed", "classcatena_1_1REST_1_1Connect.html#aef5391b820b70cdd9666db03b85e0dc6", null ]
 ];
