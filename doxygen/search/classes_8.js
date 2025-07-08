d8fd50ac1711ae1666a46ed00b5fdad73aeddfd7
ZuhayrSarkerRV <ZuhayrSarkerRV@users.noreply.github.com>
2025-07-08 21:30:01 +0000

    deploy: 0cd23157e7d0f5bc0efa5c66af39ed15d1886cad

--- doxygen/search/classes_8.js
+++ doxygen/search/classes_8.js
@@ -4,6 +4,5 @@ var searchData=
   ['languagepackrequest_1',['LanguagePackRequest',['../classcatena_1_1gRPC_1_1LanguagePackRequest.html',1,'catena::gRPC']]],
   ['languagepacktag_2',['LanguagePackTag',['../structcatena_1_1common_1_1LanguagePackTag.html',1,'catena::common']]],
   ['languages_3',['Languages',['../classcatena_1_1REST_1_1Languages.html',1,'catena::REST']]],
-  ['listlanguages_4',['ListLanguages',['../classcatena_1_1gRPC_1_1ListLanguages.html',1,'catena::gRPC']]],
-  ['logger_5',['Logger',['../classLogger.html',1,'']]]
+  ['listlanguages_4',['ListLanguages',['../classcatena_1_1gRPC_1_1ListLanguages.html',1,'catena::gRPC']]]
 ];
