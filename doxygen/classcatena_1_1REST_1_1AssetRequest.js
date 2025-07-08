7968b0410b771c6027e2532248f26d5ea277dd99
mejohnnaylor <mejohnnaylor@users.noreply.github.com>
2025-07-08 16:37:31 +0000

    deploy: 9758154797e40004fb853d34fbea438f88cd9dfe

--- doxygen/classcatena_1_1REST_1_1AssetRequest.js
+++ doxygen/classcatena_1_1REST_1_1AssetRequest.js
@@ -1,5 +1,6 @@
 var classcatena_1_1REST_1_1AssetRequest =
 [
     [ "AssetRequest", "classcatena_1_1REST_1_1AssetRequest.html#a2dc49b17cdae8a7cc4bf46269707a5ee", null ],
+    [ "finish", "classcatena_1_1REST_1_1AssetRequest.html#ab54dd1582bd7bd946fce1fddf0b7632d", null ],
     [ "proceed", "classcatena_1_1REST_1_1AssetRequest.html#a55d2f30ac4a93ba98490c15f86897f9e", null ]
 ];
