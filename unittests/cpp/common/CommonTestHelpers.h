895f5d964b0404faaa93f61602115908222ded31
Zuhayr Sarker <zuhayr.sarker@rossvideo.com>
2025-07-09 15:45:34 -0400

    okay, not EVERY space

--- unittests/cpp/common/CommonTestHelpers.h
+++ unittests/cpp/common/CommonTestHelpers.h
@@ -1,39 +1,39 @@
 /*
-* Copyright 2025 Ross Video Ltd
-*
-* Redistribution and use in source and binary forms, with or without
-* modification, are permitted provided that the following conditions are met:
-*
-* 1. Redistributions of source code must retain the above copyright notice,
-* this list of conditions and the following disclaimer.
-*
-* 2. Redistributions in binary form must reproduce the above copyright notice,
-* this list of conditions and the following disclaimer in the documentation
-* and/or other materials provided with the distribution.
-*
-* 3. Neither the name of the copyright holder nor the names of its
-* contributors may be used to endorse or promote products derived from this
-* software without specific prior written permission.
-*
-* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-* RE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-* LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-* CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-* SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-* INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-* CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-* ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-* POSSIBILITY OF SUCH DAMAGE.
-*/
+ * Copyright 2025 Ross Video Ltd
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * RE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 
 /**
-* @brief A collection of helper classes used across the tests.
-* @author zuhayr.sarker@rossvideo.com
-* @date 25/05/16
-* @copyright Copyright © 2025 Ross Video Ltd
-*/
+ * @brief A collection of helper classes used across the tests.
+ * @author zuhayr.sarker@rossvideo.com
+ * @date 25/05/16
+ * @copyright Copyright © 2025 Ross Video Ltd
+ */
 
 #pragma once
 
@@ -46,14 +46,14 @@ namespace catena {
 namespace common {
 
 /**
-* @brief Helper function to set up common mock parameter expectations
-* @param param The mock parameter to set up
-* @param oid The OID to return
-* @param descriptor The descriptor to return
-* @param isArray Whether this is an array type (defaults to false)
-* @param size The array size if it's an array type (defaults to 0)
-* @param scope The scope to return (defaults to monitor)
-*/
+ * @brief Helper function to set up common mock parameter expectations
+ * @param param The mock parameter to set up
+ * @param oid The OID to return
+ * @param descriptor The descriptor to return
+ * @param isArray Whether this is an array type (defaults to false)
+ * @param size The array size if it's an array type (defaults to 0)
+ * @param scope The scope to return (defaults to monitor)
+ */
 inline void setupMockParam(MockParam* param, const std::string& oid, MockParamDescriptor* descriptor, bool isArray = false, uint32_t size = 0, const std::string& scope = Scopes().getForwardMap().at(Scopes_e::kMonitor)) {
     EXPECT_CALL(*param, getOid())
         .WillRepeatedly(::testing::ReturnRef(oid));
@@ -84,10 +84,10 @@ public:
     };
 
     /**
-    * @brief Creates a descriptor with the given OID and sets up its mock behavior
-    * @param oid The OID to return
-    * @return A DescriptorInfo struct containing the descriptor and its sub-parameters
-    */
+     * @brief Creates a descriptor with the given OID and sets up its mock behavior
+     * @param oid The OID to return
+     * @return A DescriptorInfo struct containing the descriptor and its sub-parameters
+     */
     static DescriptorInfo createDescriptor(const std::string& oid) {
         DescriptorInfo info;
         info.oid = oid; 
@@ -100,11 +100,11 @@ public:
     }
 
     /**
-    * @brief Adds a child parameter to a parent descriptor
-    * @param parent The parent descriptor
-    * @param name The name of the child parameter
-    * @param child The child descriptor
-    */
+     * @brief Adds a child parameter to a parent descriptor
+     * @param parent The parent descriptor
+     * @param name The name of the child parameter
+     * @param child The child descriptor
+     */
     static void addChild(DescriptorInfo& parent, const std::string& name, DescriptorInfo& child) {
         parent.subParams->emplace(name, child.descriptor.get());
     }
