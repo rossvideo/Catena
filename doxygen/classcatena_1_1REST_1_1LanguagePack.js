2e63e4dcc1a49ac26dc9f200e4363ce5d0fbd944
BenWhitten-RV <BenWhitten-RV@users.noreply.github.com>
2025-07-09 20:01:52 +0000

    deploy: f8b8215cd52aac38e137d4aa49fdc6da16c17b95

--- doxygen/classcatena_1_1REST_1_1LanguagePack.js
+++ doxygen/classcatena_1_1REST_1_1LanguagePack.js
@@ -1,6 +1,5 @@
 var classcatena_1_1REST_1_1LanguagePack =
 [
     [ "LanguagePack", "classcatena_1_1REST_1_1LanguagePack.html#aa1a7004b513af79c6984db19b5ce11e4", null ],
-    [ "finish", "classcatena_1_1REST_1_1LanguagePack.html#ad1f54486c2c01affbb9e61414d1db589", null ],
     [ "proceed", "classcatena_1_1REST_1_1LanguagePack.html#a3e4de6d392e27b195852e6ea27c20eac", null ]
 ];
