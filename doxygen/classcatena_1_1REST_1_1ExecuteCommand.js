32b72af9fd703d4266c0c778e15b363e4b6a5a63
BenWhitten-RV <BenWhitten-RV@users.noreply.github.com>
2025-07-16 13:08:16 +0000

    deploy: 647f24ba61a6bccf1729b937f512cc27376ac081

--- doxygen/classcatena_1_1REST_1_1ExecuteCommand.js
+++ doxygen/classcatena_1_1REST_1_1ExecuteCommand.js
@@ -1,6 +1,5 @@
 var classcatena_1_1REST_1_1ExecuteCommand =
 [
     [ "ExecuteCommand", "classcatena_1_1REST_1_1ExecuteCommand.html#a82da7334916cf58649516b2566cde175", null ],
-    [ "finish", "classcatena_1_1REST_1_1ExecuteCommand.html#aafddb314b7aaee910f1fb16ebc9bfc95", null ],
     [ "proceed", "classcatena_1_1REST_1_1ExecuteCommand.html#a0e34f0a06c9031559191c119ab371833", null ]
 ];
