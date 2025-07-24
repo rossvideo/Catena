8cb031f45b7700d394a83be0656beebf47369ad2
ZuhayrSarkerRV <ZuhayrSarkerRV@users.noreply.github.com>
2025-07-24 16:39:27 +0000

    deploy: 3881db4756ea983b8d0a5e79980e091331c0e9d5

--- doxygen/IConnectionQueue_8h.js
+++
@@ -1,4 +0,0 @@
-var IConnectionQueue_8h =
-[
-    [ "catena::common::IConnectionQueue", "classcatena_1_1common_1_1IConnectionQueue.html", "classcatena_1_1common_1_1IConnectionQueue" ]
-];
--- doxygen/classcatena_1_1common_1_1IConnectionQueue.js
+++
@@ -1,6 +0,0 @@
-var classcatena_1_1common_1_1IConnectionQueue =
-[
-    [ "deregisterConnection", "classcatena_1_1common_1_1IConnectionQueue.html#a57258bd6a9e68c4764a614d78793fc52", null ],
-    [ "operator=", "classcatena_1_1common_1_1IConnectionQueue.html#a4b6acc4fa37b98ac9dc3ec31e696f83d", null ],
-    [ "registerConnection", "classcatena_1_1common_1_1IConnectionQueue.html#a45432af9cafbf393c8249b4800cea9ee", null ]
-];
