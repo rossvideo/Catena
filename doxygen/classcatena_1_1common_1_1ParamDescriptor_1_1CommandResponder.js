888a5a2369a93f3c27d721321cffcbfa64db3f39
ctwarog-ross <ctwarog-ross@users.noreply.github.com>
2025-08-27 17:07:20 +0000

    deploy: e38e6beb728e5c5799691f1acc2fb36af5c22428

--- doxygen/classcatena_1_1common_1_1ParamDescriptor_1_1CommandResponder.js
+++ doxygen/classcatena_1_1common_1_1ParamDescriptor_1_1CommandResponder.js
@@ -1,6 +1,6 @@
 var classcatena_1_1common_1_1ParamDescriptor_1_1CommandResponder =
 [
     [ "promise_type", "structcatena_1_1common_1_1ParamDescriptor_1_1CommandResponder_1_1promise__type.html", "structcatena_1_1common_1_1ParamDescriptor_1_1CommandResponder_1_1promise__type" ],
-    [ "getNext", "classcatena_1_1common_1_1ParamDescriptor_1_1CommandResponder.html#a8b8297eda6694095bb9cd0cb9ceca8fb", null ],
+    [ "getNext", "classcatena_1_1common_1_1ParamDescriptor_1_1CommandResponder.html#a47fb434a529bdbdd5323e8d2c57d6188", null ],
     [ "hasMore", "classcatena_1_1common_1_1ParamDescriptor_1_1CommandResponder.html#ad2ef0e58e3d142849a0e2c9dd7801891", null ]
 ];
--- doxygen/search/functions_16.js
+++ doxygen/search/functions_16.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['yield_5fvalue_0',['yield_value',['../structcatena_1_1common_1_1Device_1_1DeviceSerializer_1_1promise__type.html#afe74b859a902428ccdd761a8ffc9c601',1,'catena::common::Device::DeviceSerializer::promise_type::yield_value()'],['../structcatena_1_1common_1_1ParamDescriptor_1_1CommandResponder_1_1promise__type.html#a00388cdb351a0c0f8fc1e1b29a8891d1',1,'catena::common::ParamDescriptor::CommandResponder::promise_type::yield_value()']]]
+  ['yield_5fvalue_0',['yield_value',['../structcatena_1_1common_1_1Device_1_1DeviceSerializer_1_1promise__type.html#a7575df9e87bf178a8c19e1fab7a76f64',1,'catena::common::Device::DeviceSerializer::promise_type::yield_value()'],['../structcatena_1_1common_1_1ParamDescriptor_1_1CommandResponder_1_1promise__type.html#a6752e5e300d8fe2b8077ca90fd9afe98',1,'catena::common::ParamDescriptor::CommandResponder::promise_type::yield_value()']]]
 ];
