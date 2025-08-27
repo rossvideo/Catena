f2a4f943a24bf10ec3e1290fad5cbd40f5e9d3bc
ctwarog-ross <christian.twarog@rossvideo.com>
2025-08-27 15:20:27 -0400

    Update docker/example-healthcheck.sh
    Co-authored-by: Andrew Brown <andrew.brown@rossvideo.com>
    Signed-off-by: ctwarog-ross <christian.twarog@rossvideo.com>

--- docker/example-healthcheck.sh
+++ docker/example-healthcheck.sh
@@ -3,7 +3,7 @@
 if [ $CATENA_CONNECTION = "gRPC" ]; then
     exec curl -f -X POST --http2-prior-knowledge http://localhost:$CATENA_PORT || exit 1
 elif [ $CATENA_CONNECTION = "REST" ]; then
-    exec wget -q --spider http://localhost:$CATENA_PORT/st2138-api/v1/health || exit 1
+    exec curl -f http://localhost:$CATENA_PORT/st2138-api/v1/health || exit 1
 else
     echo "Unknown connection type $CATENA_CONNECTION, exiting."
     exit 1
