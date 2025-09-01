23bc4e80ce320e362ce396cc5b2a30bad5d9af8f
John Naylor <jnaylor@rossvideo.com>
2025-09-01 13:43:48 -0500

    avoided warning about result of expression being discarded

--- sdks/cpp/connections/REST/src/SocketReader.cpp
+++ sdks/cpp/connections/REST/src/SocketReader.cpp
@@ -58,7 +58,7 @@ void SocketReader::read(tcp::socket& socket) {
                     i++;
                 }
                 // Everything after the endpoint is the fqoid.
-                for (i; i < path.size(); i++) {
+                for (; i < path.size(); i++) {
                     fqoid_ += "/" + path.at(i);
                 }
             }
