f888f70ffba76324096ee348ec9af9a339233b0d
nelgd <nelgd@users.noreply.github.com>
2025-09-29 14:08:20 +0000

    deploy: fb46841527a810addbfcfabb56bc8dc024a11b59

--- doxygen/search/variables_5.js
+++ doxygen/search/variables_5.js
@@ -1,6 +1,4 @@
 var searchData=
 [
-  ['hasupdate_5f_0',['hasUpdate_',['../classcatena_1_1common_1_1Connect.html#aeb08058009d6f93da96fc9c72ba6c99c',1,'catena::common::Connect']]],
-  ['heartbeat_5f_1',['heartbeat_',['../classcatena_1_1common_1_1Device.html#af215801aedb611651778294be663c95c',1,'catena::common::Device']]],
-  ['heartbeatparam_5f_2',['heartbeatParam_',['../classcatena_1_1common_1_1Device.html#a56e984058804256180cf787179a777dd',1,'catena::common::Device']]]
+  ['hasupdate_5f_0',['hasUpdate_',['../classcatena_1_1common_1_1Connect.html#aeb08058009d6f93da96fc9c72ba6c99c',1,'catena::common::Connect']]]
 ];
