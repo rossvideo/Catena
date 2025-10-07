7e5e28372f28504a09825eac6b9139552b56bdb7
nelgd <nelson.daniels@rossvideo.com>
2025-10-07 19:01:22 +0000

    Swapped what I have been working on to new branch

---
+++ sdks/cpp/connections/gRPC/examples/dashboard_audiodeck/static/input.grid.js
@@ -0,0 +1,28 @@
+var inverted = false;
+var gain = 0;
+var v = 0
+
+function updateMeter(baseOID) {
+    var min = -10, max = 10;
+    v = (min + Math.random() * (max - min)) + gain;
+
+    if (inverted) {
+        v = -v;
+    }
+    params.setValue(baseOID + ".input", 0, v);
+    setRawInput();
+}
+
+function setInverted() {
+    inverted = !inverted;
+}
+
+function setGain(value) {
+    gain = value;
+}
+  
+function setRawInput() {
+    params.setValue("raw_input", 0, Math.round(v * 10) / 10);
+    params.setValue("raw_gain", 0, Math.round(gain * 10) / 10);
+    params.setValue("raw_inverted", 0, inverted);
+}
