1678b31e7b5148873aca98a5cc45b43340adc0e4
abrown-RV <abrown-RV@users.noreply.github.com>
2025-10-07 19:34:11 +0000

    deploy: 6b4e0138b51da633f5189c7ad24701ad3ecc943c

--- doxygen/search/classes_d.js
+++ doxygen/search/classes_d.js
@@ -1,6 +1,4 @@
 var searchData=
 [
-  ['rangeconstraint_0',['RangeConstraint',['../classcatena_1_1common_1_1RangeConstraint.html',1,'catena::common']]],
-  ['registrycandidate_1',['RegistryCandidate',['../structcatena_1_1common_1_1INmosNode_1_1RegistryCandidate.html',1,'catena::common::INmosNode']]],
-  ['registryselection_2',['RegistrySelection',['../structcatena_1_1common_1_1INmosNode_1_1RegistrySelection.html',1,'catena::common::INmosNode']]]
+  ['rangeconstraint_0',['RangeConstraint',['../classcatena_1_1common_1_1RangeConstraint.html',1,'catena::common']]]
 ];
