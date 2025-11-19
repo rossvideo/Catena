0290df010afe2164bec111d9a711686e39d46446
Nelson <nelson.daniels@rossvideo.com>
2025-11-19 12:33:14 -0500

    Simple Dashboard AudioDeck example (#1107)
    Added new simple dashboard audio deck example
    
    Signed-off-by: Nelson <nelson.daniels@rossvideo.com>
    Signed-off-by: Andrew Brown <andrew.brown@rossvideo.com>
    Co-authored-by: Andrew Brown <andrew.brown@rossvideo.com>
    Co-authored-by: ctwarog-ross <christian.twarog@rossvideo.com>

---
+++ sdks/cpp/connections/gRPC/examples/simple_dashboard_audiodeck/static/input.grid.js
@@ -0,0 +1,24 @@
+var v = 0
+var gain = 0;
+
+function updateMeter(baseOID) {
+    var gain = params.getValue(baseOID + ".gain", 0);
+    //var inverted = params.getValue(baseOID + ".inverted", 0);
+    
+    var min = -10, max = 10;
+    v = ((min + 3)+ Math.random() * (7)) + gain;
+
+    if (getInverted(baseOID) == 1) {
+        v = -v;
+    }
+
+    params.setValue(baseOID + ".input", 0, v);
+}
+
+function getInverted(baseOID) {
+    return params.getValue(baseOID + ".invert", 0);
+}
+
+function setStereo(value) {
+    stereo = value;
+}
