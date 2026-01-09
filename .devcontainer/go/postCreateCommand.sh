85748805d31542c92529298a708464fd1de70b83
ctwarog-ross <christian.twarog@rossvideo.com>
2026-01-09 14:31:06 -0500

    Added postCreateCommand script to be shared by devcontainer.json and git workflows

---
+++ .devcontainer/go/postCreateCommand.sh
@@ -0,0 +1,24 @@
+set -e
+
+USER_NAME=$1
+BUILD_TARGET=$2
+ROOT_DIR=$3
+
+git submodule update --init --recursive
+cd ${ROOT_DIR}/sdks/go
+make protos
+
+cd ${ROOT_DIR}
+rm -rf ${ROOT_DIR}/go.work ${ROOT_DIR}/go.sum
+go work init ./sdks/go ./build/go
+go work edit -go=1.24
+go work edit -toolchain=go1.24.11
+go work sync
+
+sudo chown -R ${USER_NAME}:${USER_NAME} ${ROOT_DIR}/${BUILD_TARGET}
+cd ${ROOT_DIR}/sdks/go
+go mod tidy
+go mod download
+make
+echo 'Docker setup complete, please restart the dev container!'
+  
