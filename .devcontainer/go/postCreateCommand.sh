8e0bc3a470733aea4b1ec177fa35846c356ab61c
ctwarog-ross <christian.twarog@rossvideo.com>
2026-01-09 14:52:17 -0500

    fdsf

--- .devcontainer/go/postCreateCommand.sh
+++ .devcontainer/go/postCreateCommand.sh
@@ -4,9 +4,6 @@ USER_NAME=$1
 BUILD_TARGET=$2
 ROOT_DIR=$3
 
-echo ${USER_NAME}
-echo ${BUILD_TARGET}
-echo ${ROOT_DIR}
 git submodule update --init --recursive
 cd ${ROOT_DIR}/sdks/go
 make protos
@@ -18,7 +15,7 @@ go work edit -go=1.24
 go work edit -toolchain=go1.24.11
 go work sync
 
-sudo chown -R ${USER_NAME}:${USER_NAME} ${ROOT_DIR}/${BUILD_TARGET}
+sudo chown -R ${USER_NAME}:${USER_NAME} ${BUILD_TARGET}
 cd ${ROOT_DIR}/sdks/go
 go mod tidy
 go mod download
