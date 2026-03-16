5b89344fba6577e511b1af9b8c58064dc2449c63
Andrew Brown <andrew.brown@rossvideo.com>
2026-03-16 19:46:00 +0000

    gitignore goooood

--- scripts/make_cert.sh
+++ scripts/make_cert.sh
@@ -1,6 +1,10 @@
-openssl req -x509 -out localhost.crt -keyout localhost.key \
+dirpath=$(dirname "$0")
+certs="$dirpath/../certs"
+mkdir -p "$certs"
+
+openssl req -x509 -out $certs/localhost.crt -keyout $certs/localhost.key \
   -newkey rsa:2048 -nodes -sha256 \
   -subj '/CN=localhost' -extensions EXT -config <( \
    printf "[dn]\nCN=localhost\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:localhost\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
 
-echo "RUN with these flags: --secure_comms tls --certs . --cert_file localhost.crt --key_file localhost.key"
+echo "RUN with these flags: --secure_comms tls --certs certs/ --cert_file localhost.crt --key_file localhost.key"
