72fa89d1618885945e527726e53a3018805ec3a4
KeonF-ross <keon.foster@rossvideo.com>
2026-04-21 17:19:54 +0000

    Fixed braces

--- sdks/cpp/common/src/NmosNode.cpp
+++ sdks/cpp/common/src/NmosNode.cpp
@@ -385,15 +385,16 @@ void NmosNode::browse_cb(
 {
     NmosNode* node = static_cast<NmosNode*>(userdata);
     switch (event) {
-        case AVAHI_BROWSER_NEW: 
+        case AVAHI_BROWSER_NEW:
             LOG(DEBUG) << "Discovered service: " << name << " of type " << type << " in domain " << domain;
             // Found a new service, resolve it
             if (!(avahi_service_resolver_new(
                 node->getClient(), interface, protocol, name, type, domain,
-                AVAHI_PROTO_UNSPEC, (AvahiLookupFlags)0, resolve_cb, node)))
-            LOG(ERROR) << "Failed to resolve service '" << name << "': " << avahi_strerror(avahi_client_errno(node->getClient()));
+                AVAHI_PROTO_UNSPEC, (AvahiLookupFlags)0, resolve_cb, node))) {
+                LOG(ERROR) << "Failed to resolve service '" << name << "': " << avahi_strerror(avahi_client_errno(node->getClient()));
+            }
             break;
-        case AVAHI_BROWSER_FAILURE: 
+        case AVAHI_BROWSER_FAILURE:
             LOG(ERROR) << "browse failure: " << avahi_strerror(avahi_client_errno(node->getClient())) << "\n";
             avahi_simple_poll_quit(node->getPoll());
             break;
