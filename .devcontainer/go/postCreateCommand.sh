6043e6e61fc39262f892e80dbc2a38833b2dabd9
Andrew Brown <andrew.brown@rossvideo.com>
2026-05-01 17:51:01 +0000

    fix the cur dir

--- .devcontainer/go/postCreateCommand.sh
+++ .devcontainer/go/postCreateCommand.sh
@@ -7,8 +7,12 @@ ROOT_DIR=$3
 cd ${ROOT_DIR}/sdks/go
 make protos-all
 
+cd ${ROOT_DIR}
+# should exist cause of volume mount, but just in case
 mkdir -p ${BUILD_TARGET}
+# volume mount might belong to root
 sudo chown -R ${USER_NAME}:${USER_NAME} ${BUILD_TARGET}
+
 cd ${ROOT_DIR}/sdks/go
 go mod tidy
 go mod download
